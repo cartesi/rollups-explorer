39e35b0e2e96e52b067dbed38047d30982602a43
Bruno Menezes <brunodmenezes@gmail.com>
2023-09-26 12:20:28 +1300

    Add Vitest + support libraries, including configs to the UI package.

---
+++ packages/ui/vitest-setup.ts
@@ -0,0 +1,23 @@
+import { Globals } from "@react-spring/web";
+import "@testing-library/jest-dom/vitest";
+import { beforeAll, vi } from "vitest";
+
+Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: vi.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: vi.fn(), // deprecated
+        removeListener: vi.fn(), // deprecated
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn(),
+        dispatchEvent: vi.fn(),
+    })),
+});
+
+beforeAll(() => {
+    Globals.assign({
+        skipAnimation: true,
+    });
+});
