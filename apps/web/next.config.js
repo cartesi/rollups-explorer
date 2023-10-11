275c9bea00556f97ddd816b2310c8a19f2ff746a
Bruno Menezes <brunodmenezes@gmail.com>
2023-10-11 13:40:48 +1300

    feat: Add CSP header config to support integration with safe.global.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,41 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';  
+  font-src 'self';
+  frame-ancestors 'self' https://app.safe.global;
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     output: "standalone",
     basePath: process.env.BASE_PATH || "",
+    async headers() {
+        return [
+            {
+                source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        value: `frame-ancestors 'self' https://app.safe.global;`,
+                    },
+                ],
+            },
+            {
+                source: "/manifest.json",
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        value: ContentSecurityPolicy.replace(
+                            /\s{2,}/g,
+                            " ",
+                        ).trim(),
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
