fb033b87bcab5ea3117c1ae2af412acf028be27c
Bruno Menezes <brunodmenezes@gmail.com>
2023-10-30 22:15:50 +1300

    feat: Update modal theme through the theme-creator.

--- apps/web/src/providers/theme.tsx
+++ apps/web/src/providers/theme.tsx
@@ -1,6 +1,18 @@
-import { MantineThemeOverride, createTheme } from "@mantine/core";
+import { MantineThemeOverride, Modal, createTheme } from "@mantine/core";
 
 export const theme: MantineThemeOverride = createTheme({
     fontFamily: "Open Sans, sans-serif",
     primaryColor: "cyan",
+    components: {
+        Modal: Modal.extend({
+            defaultProps: {
+                size: "lg",
+                centered: true,
+                overlayProps: {
+                    backgroundOpacity: 0.55,
+                    blur: 3,
+                },
+            },
+        }),
+    },
 });
