c9e96e0951595a3eed1a19937fd97dd7be0e499f
nevendiulgerov <neven.diulgerov@gmail.com>
2023-11-03 09:28:57 +0200

    test(packages/ui): Fix failing tests

--- packages/ui/test/RawInputForm.test.tsx
+++ packages/ui/test/RawInputForm.test.tsx
@@ -1,11 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { afterAll, describe, it } from "vitest";
-import { RawInputForm, ApplicationAutocomplete } from "../src/RawInputForm";
+import { RawInputForm } from "../src/RawInputForm";
 import withMantineTheme from "./utils/WithMantineTheme";
-
-const ApplicationAutoCompleteComponent = withMantineTheme(
-    ApplicationAutocomplete,
-);
 
 const Component = withMantineTheme(RawInputForm);
 
@@ -14,12 +10,6 @@ const applications = [
     "0x70ac08179605af2d9e75782b8decdd3c22aa4d0c",
     "0x71ab24ee3ddb97dc01a161edf64c8d51102b0cd3",
 ];
-
-const defaultApplicationProps = {
-    applications,
-    application: applications[0],
-    onChange: () => undefined,
-};
 
 const defaultProps = {
     applications,
@@ -41,6 +31,14 @@ vi.mock("@cartesi/rollups-wagmi", async () => {
 vi.mock("wagmi", async () => {
     return {
         useWaitForTransaction: () => ({}),
+    };
+});
+
+vi.mock("viem", async () => {
+    const actual = await vi.importActual("viem");
+    return {
+        ...(actual as any),
+        getAddress: (address: string) => address,
     };
 });
 
@@ -75,6 +73,8 @@ describe("Rollups RawInputForm", () => {
                     value: "",
                 },
             });
+
+            fireEvent.blur(textarea);
 
             expect(textarea.getAttribute("aria-invalid")).toBe("true");
             expect(screen.getByText("Invalid hex string")).toBeInTheDocument();
@@ -135,21 +135,13 @@ describe("Rollups RawInputForm", () => {
 
     describe("ApplicationAutocomplete", () => {
         it("should display correct label", () => {
-            render(
-                <ApplicationAutoCompleteComponent
-                    {...defaultApplicationProps}
-                />,
-            );
+            render(<Component {...defaultProps} />);
 
             expect(screen.getByText("Application")).toBeInTheDocument();
         });
 
         it("should display correct description", () => {
-            render(
-                <ApplicationAutoCompleteComponent
-                    {...defaultApplicationProps}
-                />,
-            );
+            render(<Component {...defaultProps} />);
 
             expect(
                 screen.getByText("The application smart contract address"),
@@ -157,40 +149,25 @@ describe("Rollups RawInputForm", () => {
         });
 
         it("should display correct placeholder", () => {
-            const { container } = render(
-                <ApplicationAutoCompleteComponent
-                    {...defaultApplicationProps}
-                />,
-            );
+            const { container } = render(<Component {...defaultProps} />);
             const input = container.querySelector("input");
 
             expect(input?.getAttribute("placeholder")).toBe("0x");
         });
 
         it("should display alert for unemployed application", () => {
-            render(
-                <ApplicationAutoCompleteComponent
-                    {...defaultApplicationProps}
-                    application="undeployed-application"
-                />,
-            );
+            const { container } = render(<Component {...defaultProps} />);
+            const input = container.querySelector("input") as HTMLInputElement;
+
+            fireEvent.change(input, {
+                target: {
+                    value: "0x60a7048c3136293071605a4eaffef49923e981fe",
+                },
+            });
 
             expect(
                 screen.getByText("This is an undeployed application."),
             ).toBeInTheDocument();
-        });
-
-        it("should should set input value to selected application", () => {
-            const selectedApplication = applications[1];
-            const { container } = render(
-                <ApplicationAutoCompleteComponent
-                    {...defaultApplicationProps}
-                    application={selectedApplication}
-                />,
-            );
-            const input = container.querySelector("input");
-
-            expect(input?.getAttribute("value")).toBe(selectedApplication);
         });
     });
 });
