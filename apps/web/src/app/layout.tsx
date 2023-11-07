544aef1f45f4dae1af127d8812bad201abc63ab0
Bruno Menezes <brunodmenezes@gmail.com>
2023-11-07 16:35:47 +1300

    refactor: Keep Layout as server-comp and move providers down the tree.
    Change based on nextJS suggestions: https://nextjs.org/docs/app/building-your-application/rendering/composition-patterns#moving-client-components-down-the-tree

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,13 +1,10 @@
-"use client";
-import { ColorSchemeScript } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
+
 import React, { FC } from "react";
 
-import { ConnectionConfigProvider } from "../providers/connectionConfig/connectionConfigProvider";
-import GraphQLProvider from "../providers/graphqlProvider";
-import StyleProvider from "../providers/styleProvider";
-import WalletProvider from "../providers/walletProvider";
+import { ColorSchemeScript } from "@mantine/core";
+import { Providers } from "../providers/providers";
 import Shell from "./shell";
 
 const Layout: FC<{ children: React.ReactNode }> = ({ children }) => {
@@ -18,15 +15,9 @@ const Layout: FC<{ children: React.ReactNode }> = ({ children }) => {
                 <link rel="shortcut icon" href="/favicon.ico" />
             </head>
             <body>
-                <StyleProvider>
-                    <WalletProvider>
-                        <GraphQLProvider>
-                            <ConnectionConfigProvider>
-                                <Shell>{children}</Shell>
-                            </ConnectionConfigProvider>
-                        </GraphQLProvider>
-                    </WalletProvider>
-                </StyleProvider>
+                <Providers>
+                    <Shell>{children}</Shell>
+                </Providers>
             </body>
         </html>
     );
