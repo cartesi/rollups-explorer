27b7c47830c765f2356157416aefe1d6d624c13d
Bruno Menezes <brunodmenezes@gmail.com>
2023-11-14 10:45:56 +1300

    chore: Replace Subsquid API address.

--- apps/web/codegen.ts
+++ apps/web/codegen.ts
@@ -1,8 +1,6 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
-const schema = "https://squid.subsquid.io/cartesi-rollups-mainnet/graphql";
-// const schema = "https://squid.subsquid.io/cartesi-rollups-sepolia/graphql";
-// const schema = "http://127.0.0.1:4350/graphql";
+const schema = "https://mainnet.api.cartesiscan.io/graphql";
 
 const plugins = [
     {
