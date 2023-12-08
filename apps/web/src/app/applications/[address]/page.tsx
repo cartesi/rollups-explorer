32bd3759e88ed379b22c47d84c2df36d213b9769
Neven Dyulgerov <neven.diulgerov@gmail.com>
2023-12-08 17:27:55 +0200

    feat(apps/web): Render pages server-side and add metadata

--- apps/web/src/app/applications/[address]/page.tsx
+++ apps/web/src/app/applications/[address]/page.tsx
@@ -1,120 +1,47 @@
-"use client";
-import {
-    Anchor,
-    Breadcrumbs,
-    Group,
-    Pagination,
-    Select,
-    Stack,
-    Text,
-    Title,
-} from "@mantine/core";
-import { useScrollIntoView } from "@mantine/hooks";
-import Link from "next/link";
-import { pathOr } from "ramda";
-import { FC, useEffect, useState } from "react";
+import { Group, Stack, Title } from "@mantine/core";
+import { FC } from "react";
+import { Metadata } from "next";
 import { TbInbox } from "react-icons/tb";
 import Address from "../../../components/address";
-import { InputOrderByInput, useInputsQuery } from "../../../graphql";
-import {
-    limitBounds,
-    usePaginationParams,
-} from "../../../hooks/usePaginationParams";
-import InputsTable from "../../../components/inputsTable";
+import Inputs from "../../../components/inputs";
+import Breadcrumbs from "../../../components/breadcrumbs";
 
 export type ApplicationPageProps = {
     params: { address: string };
 };
+
+export async function generateMetadata({
+    params,
+}: ApplicationPageProps): Promise<Metadata> {
+    return {
+        title: `Application ${params.address}`,
+    };
+}
+
 const ApplicationPage: FC<ApplicationPageProps> = ({ params }) => {
-    const [{ limit, page }, updateParams] = usePaginationParams();
-    const after = page === 1 ? undefined : ((page - 1) * limit).toString();
-    const [{ data, fetching }] = useInputsQuery({
-        variables: {
-            orderBy: InputOrderByInput.TimestampDesc,
-            applicationId: params.address.toLowerCase(),
-            limit,
-            after,
-        },
-    });
-    const totalInputs = data?.inputsConnection.totalCount ?? 1;
-    const totalPages = Math.ceil(totalInputs / limit);
-    const [activePage, setActivePage] = useState(
-        page > totalPages ? totalPages : page,
-    );
-    const inputs = data?.inputsConnection.edges.map((edge) => edge.node) ?? [];
-    const { scrollIntoView, targetRef } = useScrollIntoView<HTMLDivElement>({
-        duration: 700,
-        offset: 150,
-        cancelable: true,
-    });
-
-    if (!fetching && page > totalPages) {
-        updateParams(totalPages, limit);
-    }
-
-    useEffect(() => {
-        setActivePage((n) => {
-            return n !== page ? page : n;
-        });
-    }, [page]);
-
     return (
         <Stack>
-            <Breadcrumbs>
-                <Anchor href="/" component={Link}>
-                    Home
-                </Anchor>
-                <Anchor href="/applications" component={Link}>
-                    Applications
-                </Anchor>
+            <Breadcrumbs
+                breadcrumbs={[
+                    {
+                        href: "/",
+                        label: "Home",
+                    },
+                    {
+                        href: "/applications",
+                        label: "Applications",
+                    },
+                ]}
+            >
                 <Address value={params.address as Address} icon />
             </Breadcrumbs>
-            <Group ref={targetRef}>
+
+            <Group>
                 <TbInbox size={40} />
                 <Title order={2}>Inputs</Title>
             </Group>
-            <Stack>
-                <Pagination
-                    styles={{ root: { alignSelf: "flex-end" } }}
-                    value={activePage}
-                    total={totalPages}
-                    onChange={(pageN) => {
-                        updateParams(pageN, limit);
-                    }}
-                />
 
-                <InputsTable inputs={inputs} />
-
-                <Group justify="space-between" align="center">
-                    <Group>
-                        <Text>Show:</Text>
-                        <Select
-                            style={{ width: "5rem" }}
-                            value={limit.toString()}
-                            onChange={(val) => {
-                                const entry = val ?? limit;
-                                const l = pathOr(limit, [entry], limitBounds);
-                                updateParams(page, l);
-                            }}
-                            data={[
-                                limitBounds[10].toString(),
-                                limitBounds[20].toString(),
-                                limitBounds[30].toString(),
-                            ]}
-                        />
-                        <Text>inputs</Text>
-                    </Group>
-                    <Pagination
-                        styles={{ root: { alignSelf: "flex-end" } }}
-                        value={activePage}
-                        total={totalPages}
-                        onChange={(pageN) => {
-                            updateParams(pageN, limit);
-                            scrollIntoView({ alignment: "center" });
-                        }}
-                    />
-                </Group>
-            </Stack>{" "}
+            <Inputs applicationId={params.address} />
         </Stack>
     );
 };
