799fca1ca7d231eb2b95b08b6dd002a520f38239
Neven Dyulgerov <neven.diulgerov@gmail.com>
2023-12-08 17:27:24 +0200

    feat(apps/web): Add base metadata

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 import React, { FC } from "react";
+import { Metadata } from "next";
 import { ColorSchemeScript } from "@mantine/core";
 import { Providers } from "../providers/providers";
 import Shell from "../components/shell";
@@ -9,7 +10,20 @@ interface LayoutProps {
     children: React.ReactNode;
 }
 
-const Layout: FC<{ children: React.ReactNode }> = ({ children }) => {
+export const metadata: Metadata = {
+    title: {
+        template: "%s | CartesiScan",
+        default: "Blockchain Explorer | CartesiScan",
+    },
+    description:
+        "CartesiScan is a tool for inspecting and analyzing Cartesi rollups applications. Blockchain explorer for Ethereum Networks.",
+};
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+const Layout: FC<LayoutProps> = ({ children }) => {
     return (
         <html lang="en">
             <head>
