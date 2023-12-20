079880a6013a3ee2c221a4d6265ade85fcdee185
Bruno Menezes <brunodmenezes@gmail.com>
2023-12-20 16:26:15 +1300

    refactor: Remove Input icon and title from Home page.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,7 @@
-import { Group, Stack, Title } from "@mantine/core";
-import { TbInbox } from "react-icons/tb";
+import { Stack } from "@mantine/core";
+import Breadcrumbs from "../components/breadcrumbs";
 import EntriesSummary from "../components/entriesSummary";
 import LatestEntries from "../components/latestEntries";
-import Breadcrumbs from "../components/breadcrumbs";
 
 export default function HomePage() {
     return (
@@ -15,11 +14,6 @@ export default function HomePage() {
                     },
                 ]}
             />
-
-            <Group>
-                <TbInbox size={40} />
-                <Title order={2}>Inputs</Title>
-            </Group>
 
             <EntriesSummary />
             <LatestEntries />
