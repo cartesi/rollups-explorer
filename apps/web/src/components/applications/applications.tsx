c4a23b93e4c18567de366c257e82db61e411013d
Neven Dyulgerov <neven.diulgerov@gmail.com>
2023-12-22 17:37:19 +0200

    feat(apps/web): Utilize Paginated component for inputs and applications components

--- apps/web/src/components/applications/applications.tsx
+++ apps/web/src/components/applications/applications.tsx
@@ -1,96 +1,71 @@
 "use client";
 
-import { Group, Pagination, Select, Stack, Text } from "@mantine/core";
-import { useScrollIntoView } from "@mantine/hooks";
-import { pathOr } from "ramda";
-import { FC, useEffect, useState } from "react";
+import { Loader, Stack, Table } from "@mantine/core";
+import { FC, useCallback, useState } from "react";
 import {
     ApplicationOrderByInput,
     useApplicationsConnectionQuery,
-} from "../../graphql";
-import {
-    limitBounds,
-    usePaginationParams,
-} from "../../hooks/usePaginationParams";
-import ApplicationsTable from "./applicationsTable";
+} from "../graphql";
+import ApplicationRow from "../components/applicationRow";
+import Paginated from "./paginated";
 
 const Applications: FC = () => {
-    const [{ limit, page }, updateParams] = usePaginationParams();
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
     const after = page === 1 ? undefined : ((page - 1) * limit).toString();
     const [query] = useApplicationsConnectionQuery({
         variables: { orderBy: ApplicationOrderByInput.IdAsc, limit, after },
     });
-    const totalInputs = query.data?.applicationsConnection.totalCount ?? 1;
-    const totalPages = Math.ceil(totalInputs / limit);
-    const [activePage, setActivePage] = useState(
-        page > totalPages ? totalPages : page,
-    );
-    const applications =
-        query.data?.applicationsConnection.edges.map((edge) => edge.node) ?? [];
-    const { scrollIntoView } = useScrollIntoView<HTMLDivElement>({
-        duration: 700,
-        offset: 150,
-        cancelable: true,
-    });
 
-    useEffect(() => {
-        if (!query.fetching && page > totalPages) {
-            updateParams(totalPages, limit);
-        }
-    }, [limit, page, query.fetching, totalPages, updateParams]);
-
-    useEffect(() => {
-        setActivePage((n) => {
-            return n !== page ? page : n;
-        });
-    }, [page]);
+    const onChangePagination = useCallback((limit: number, page: number) => {
+        setLimit(limit);
+        setPage(page);
+    }, []);
 
     return (
         <Stack>
-            <Pagination
-                styles={{ root: { alignSelf: "flex-end" } }}
-                value={activePage}
-                total={totalPages}
-                onChange={(pageN) => {
-                    updateParams(pageN, limit);
-                }}
-            />
-
-            <ApplicationsTable
-                applications={applications}
+            <Paginated
                 fetching={query.fetching}
-                totalCount={query.data?.applicationsConnection.totalCount ?? 0}
-            />
-
-            <Group justify="space-between" align="center">
-                <Group>
-                    <Text>Show:</Text>
-                    <Select
-                        style={{ width: "5rem" }}
-                        value={limit.toString()}
-                        onChange={(val) => {
-                            const entry = val ?? limit;
-                            const l = pathOr(limit, [entry], limitBounds);
-                            updateParams(page, l);
-                        }}
-                        data={[
-                            limitBounds[10].toString(),
-                            limitBounds[20].toString(),
-                            limitBounds[30].toString(),
-                        ]}
-                    />
-                    <Text>Applications</Text>
-                </Group>
-                <Pagination
-                    styles={{ root: { alignSelf: "flex-end" } }}
-                    value={activePage}
-                    total={totalPages}
-                    onChange={(pageN) => {
-                        updateParams(pageN, limit);
-                        scrollIntoView({ alignment: "center" });
-                    }}
-                />
-            </Group>
+                totalCount={query.data?.applicationsConnection.totalCount}
+                onChange={onChangePagination}
+            >
+                <Table>
+                    <Table.Thead>
+                        <Table.Tr>
+                            <Table.Th>Id</Table.Th>
+                            <Table.Th>Owner</Table.Th>
+                            <Table.Th>URL</Table.Th>
+                        </Table.Tr>
+                    </Table.Thead>
+                    <Table.Tbody>
+                        {query.fetching && (
+                            <Table.Tr>
+                                <Table.Td align="center" colSpan={3}>
+                                    <Loader />
+                                </Table.Td>
+                            </Table.Tr>
+                        )}
+                        {query.data?.applicationsConnection.totalCount ===
+                            0 && (
+                            <Table.Tr>
+                                <Table.Td colSpan={3} align="center">
+                                    No applications
+                                </Table.Td>
+                            </Table.Tr>
+                        )}
+                        {query.data?.applicationsConnection.edges.map(
+                            ({ node }) => (
+                                <ApplicationRow
+                                    key={node.id}
+                                    application={
+                                        node as Omit<Application, "inputs">
+                                    }
+                                />
+                            ),
+                        )}
+                    </Table.Tbody>
+                </Table>
+            </Paginated>
         </Stack>
     );
 };
