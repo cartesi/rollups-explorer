2473afe06c91fa0204001cbaf557ff1bf3a0e3aa
Neven Dyulgerov <neven.diulgerov@gmail.com>
2024-01-03 18:20:25 +0200

    feat(packages/ui): Export ERC721DepositForm component

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -7,6 +7,7 @@ export {
 } from "./InputDetails";
 export { ERC20DepositForm } from "./ERC20DepositForm";
 export { EtherDepositForm } from "./EtherDepositForm";
+export { ERC721DepositForm } from "./ERC721DepositForm";
 export { RawInputForm } from "./RawInputForm";
 export { Summary } from "./Summary";
 export { SummaryCard } from "./SummaryCard";
