851e6ff7184aad3d8fe8f5460a9784578be76bf7
Neven Dyulgerov <neven.diulgerov@gmail.com>
2024-02-09 17:29:46 +0200

    feat(packages/rollups-wagmi): Update rollups-wagmi package to include CartesiDApp ABI for code generation

--- packages/rollups-wagmi/wagmi.config.ts
+++ packages/rollups-wagmi/wagmi.config.ts
@@ -1,9 +1,17 @@
 import hardhatDeploy from "@sunodo/wagmi-plugin-hardhat-deploy";
 import { defineConfig } from "@wagmi/cli";
+import type { Abi } from "abitype";
 import { erc, react } from "@wagmi/cli/plugins";
+import CartesiDAppContract from "../../node_modules/@cartesi/rollups/export/artifacts/contracts/dapp/CartesiDApp.sol/CartesiDApp.json";
 
 export default defineConfig({
     out: "src/index.tsx",
+    contracts: [
+        {
+            name: "CartesiDApp",
+            abi: CartesiDAppContract.abi as Abi,
+        },
+    ],
     plugins: [
         hardhatDeploy({
             directory: "../../node_modules/@cartesi/rollups/export/abi",
