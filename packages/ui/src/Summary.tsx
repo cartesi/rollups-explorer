246af54a9d0057cf75f00ede2e07dd178e331389
dandhee.damarrama <dandhee.damarrama@gmail.com>
2024-03-05 12:40:21 +0700

    feat(packages/ui): edit summary display

--- packages/ui/src/Summary.tsx
+++ packages/ui/src/Summary.tsx
@@ -7,22 +7,31 @@ import { SummaryCard } from "./SummaryCard";
 export type SummaryProps = {
     inputs: number;
     applications: number;
+    applicationsOwned: number;
 };
 
 export const Summary: FC<SummaryProps> = ({
     inputs,
     applications,
+    applicationsOwned,
 }: SummaryProps) => {
     return (
         <Grid gutter="md">
-            <Grid.Col span={{ base: 12, md: 6 }} my="md">
+            <Grid.Col span={{ base: 12, md: 4 }} my="md">
                 <SummaryCard title="Inputs" icon={TbInbox} value={inputs} />
             </Grid.Col>
-            <Grid.Col span={{ base: 12, md: 6 }} my="md">
+            <Grid.Col span={{ base: 12, md: 4 }} my="md">
                 <SummaryCard
                     title="Applications"
                     icon={TbApps}
                     value={applications}
+                />
+            </Grid.Col>
+            <Grid.Col span={{ base: 12, md: 4 }} my="md">
+                <SummaryCard
+                    title="My Applications"
+                    icon={TbApps}
+                    value={applicationsOwned}
                 />
             </Grid.Col>
         </Grid>
