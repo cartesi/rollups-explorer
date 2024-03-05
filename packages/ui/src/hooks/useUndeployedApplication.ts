e0803c089d0c07fa9539e00d81e4c22900938abe
Neven Dyulgerov <neven.diulgerov@gmail.com>
2024-03-05 15:57:01 +0200

    chore(packages/ui): Refactor useUndeployedApplication hook

--- packages/ui/src/hooks/useUndeployedApplication.ts
+++ packages/ui/src/hooks/useUndeployedApplication.ts
@@ -1,26 +1,19 @@
-import { useEffect, useState } from "react";
 import { Address, isAddress, zeroAddress } from "viem";
+import { useDebouncedValue } from "@mantine/hooks";
 
 export default function useUndeployedApplication(
     address: Address,
     applications: string[],
     delay = 1000,
 ) {
-    const [isUndeployedApp, setUndeployedApp] = useState(false);
-
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            const isUndeployedApp =
-                isAddress(address) &&
-                address !== zeroAddress &&
-                !applications.some(
-                    (a) => a.toLowerCase() === address.toLowerCase(),
-                );
-            setUndeployedApp(isUndeployedApp);
-        }, delay);
-
-        return () => clearTimeout(timeout);
-    }, [address, applications, delay]);
+    const [isUndeployedApp] = useDebouncedValue(
+        isAddress(address) &&
+            address !== zeroAddress &&
+            !applications.some(
+                (a) => a.toLowerCase() === address.toLowerCase(),
+            ),
+        delay,
+    );
 
     return isUndeployedApp;
 }
