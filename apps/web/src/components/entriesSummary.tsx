b78d5f874190fc8b4429c648939b8f15b754b842
dandhee.damarrama <dandhee.damarrama@gmail.com>
2024-03-05 12:40:03 +0700

    feat(apps/web): add new query for applications owner

--- apps/web/src/components/entriesSummary.tsx
+++ apps/web/src/components/entriesSummary.tsx
@@ -2,15 +2,27 @@
 
 import { Summary } from "@cartesi/rollups-explorer-ui";
 import { FC } from "react";
-import { useStatsQuery } from "../graphql/explorer/hooks/queries";
-
+import { useAccount } from "wagmi";
+import {
+    useStatsApplicationsOwnerQuery,
+    useStatsQuery,
+} from "../graphql/explorer/hooks/queries";
 const EntriesSummary: FC = () => {
+    const { address } = useAccount();
     const [{ data: stats }] = useStatsQuery();
+    const [{ data: applicationsOwned }] = useStatsApplicationsOwnerQuery({
+        variables: {
+            applicationId: address,
+        },
+    });
 
     return (
         <Summary
             inputs={stats?.inputsConnection?.totalCount ?? 0}
             applications={stats?.applicationsConnection?.totalCount ?? 0}
+            applicationsOwned={
+                applicationsOwned?.applicationsConnection?.totalCount ?? 0
+            }
         />
     );
 };
