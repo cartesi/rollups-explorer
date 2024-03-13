93292e5ae91c0dedb96f51927f75eadbe760eaa5
Neven Dyulgerov <neven.diulgerov@gmail.com>
2024-03-13 21:40:00 +0200

    feat(packages/ui): Update raw input form and transaction progress

--- packages/ui/src/RawInputForm.tsx
+++ packages/ui/src/RawInputForm.tsx
@@ -1,6 +1,6 @@
 import {
-    useInputBoxAddInput,
-    usePrepareInputBoxAddInput,
+    useWriteInputBoxAddInput,
+    useSimulateInputBoxAddInput,
 } from "@cartesi/rollups-wagmi";
 import {
     Alert,
@@ -58,18 +58,23 @@ export const RawInputForm: FC<RawInputFormProps> = (props) => {
         }),
     });
     const { address, rawInput } = form.getTransformedValues();
-    const prepare = usePrepareInputBoxAddInput({
+    const prepare = useSimulateInputBoxAddInput({
         args: [address, rawInput],
-        enabled: form.isValid(),
+        query: {
+            enabled: form.isValid(),
+        },
     });
-    const execute = useInputBoxAddInput(prepare.config);
-    const wait = useWaitForTransactionReceipt(execute.data);
-    const loading = execute.status === "loading" || wait.status === "pending";
+
+    const execute = useWriteInputBoxAddInput();
+    const wait = useWaitForTransactionReceipt({
+        hash: execute.data,
+    });
+    const loading = execute.status === "pending" || wait.isLoading;
     const canSubmit = form.isValid() && prepare.error === null;
     const isUndeployedApp = useUndeployedApplication(address, applications);
 
     useEffect(() => {
-        if (wait.status === "success") {
+        if (wait.isSuccess) {
             form.reset();
             onSearchApplications("");
         }
@@ -120,7 +125,7 @@ export const RawInputForm: FC<RawInputFormProps> = (props) => {
 
                 <Collapse
                     in={
-                        execute.isLoading ||
+                        execute.status === "pending" ||
                         wait.isLoading ||
                         execute.isSuccess ||
                         execute.isError
@@ -141,7 +146,9 @@ export const RawInputForm: FC<RawInputFormProps> = (props) => {
                         disabled={!canSubmit}
                         leftSection={<TbCheck />}
                         loading={loading}
-                        onClick={() => execute.write()}
+                        onClick={() =>
+                            execute.writeContract(prepare.data!.request)
+                        }
                     >
                         Send
                     </Button>
--- packages/ui/src/TransactionStatus.ts
+++ packages/ui/src/TransactionStatus.ts
@@ -2,3 +2,9 @@ export type TransactionStageStatus = {
     status: "error" | "pending" | "success" | "idle" | "loading";
     error: Error | null;
 };
+
+export type TransactionWaitStatus = {
+    status: "error" | "pending" | "success";
+    fetchStatus: "fetching" | "idle" | "paused";
+    error: Error | null;
+};
