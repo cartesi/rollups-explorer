738b404ada10b26b4b135ec662e6f0539294778a
dandhee.damarrama <dandhee.damarrama@gmail.com>
2024-03-14 13:51:13 +0700

    feat(apps/web): make user application table overflow-y

--- apps/web/src/components/applications/userApplicationsTable.tsx
+++ apps/web/src/components/applications/userApplicationsTable.tsx
@@ -1,4 +1,4 @@
-import { Loader, Table, Text } from "@mantine/core";
+import { Box, Loader, Table, Text } from "@mantine/core";
 import { FC } from "react";
 import { Application } from "../../graphql/explorer/types";
 import { ApplicationsTableProps } from "./applicationsTable";
@@ -7,38 +7,47 @@ import UserApplicationsRow from "./userApplicationsRow";
 const UserApplicationsTable: FC<ApplicationsTableProps> = (props) => {
     const { applications, fetching, totalCount } = props;
     return (
-        <Table>
-            <Table.Thead>
-                <Table.Tr>
-                    <Table.Th>Id</Table.Th>
-                    <Table.Th>URL</Table.Th>
-                    <Table.Th>Age</Table.Th>
-                </Table.Tr>
-            </Table.Thead>
-            <Table.Tbody>
-                {fetching ? (
+        <Box
+            h={"40vh"}
+            style={{
+                overflowY: "scroll",
+            }}
+        >
+            <Table>
+                <Table.Thead>
                     <Table.Tr>
-                        <Table.Td align="center" colSpan={3}>
-                            <Loader />
-                        </Table.Td>
+                        <Table.Th>Id</Table.Th>
+                        <Table.Th>URL</Table.Th>
+                        <Table.Th>Age</Table.Th>
                     </Table.Tr>
-                ) : (
-                    totalCount === 0 && (
+                </Table.Thead>
+                <Table.Tbody>
+                    {fetching ? (
                         <Table.Tr>
-                            <Table.Td colSpan={3} align="center">
-                                <Text fw={700}>No applications</Text>
+                            <Table.Td align="center" colSpan={3}>
+                                <Loader />
                             </Table.Td>
                         </Table.Tr>
-                    )
-                )}
-                {applications.map((application) => (
-                    <UserApplicationsRow
-                        key={application.id}
-                        application={application as Omit<Application, "inputs">}
-                    />
-                ))}
-            </Table.Tbody>
-        </Table>
+                    ) : (
+                        totalCount === 0 && (
+                            <Table.Tr>
+                                <Table.Td colSpan={3} align="center">
+                                    <Text fw={700}>No applications</Text>
+                                </Table.Td>
+                            </Table.Tr>
+                        )
+                    )}
+                    {applications.map((application) => (
+                        <UserApplicationsRow
+                            key={application.id}
+                            application={
+                                application as Omit<Application, "inputs">
+                            }
+                        />
+                    ))}
+                </Table.Tbody>
+            </Table>
+        </Box>
     );
 };
 
