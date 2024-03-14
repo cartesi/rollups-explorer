1b96892010e80d3a7224b6cbbdb4cca67e19a1a1
dandhee.damarrama <dandhee.damarrama@gmail.com>
2024-03-14 13:28:37 +0700

    feat(apps/web): add options on user application query

--- packages/ui/src/Summary.tsx
+++ packages/ui/src/Summary.tsx
@@ -2,6 +2,7 @@
 import { Grid } from "@mantine/core";
 import { FC } from "react";
 import { TbApps, TbInbox } from "react-icons/tb";
+import { useAccount } from "wagmi";
 import { SummaryCard } from "./SummaryCard";
 
 export type SummaryProps = {
@@ -15,25 +16,28 @@ export const Summary: FC<SummaryProps> = ({
     applications,
     applicationsOwned,
 }: SummaryProps) => {
+    const { isConnected } = useAccount();
     return (
         <Grid gutter="md">
-            <Grid.Col span={{ base: 12, md: 4 }} my="md">
+            <Grid.Col span={{ base: 12, md: isConnected ? 4 : 6 }} my="md">
                 <SummaryCard title="Inputs" icon={TbInbox} value={inputs} />
             </Grid.Col>
-            <Grid.Col span={{ base: 12, md: 4 }} my="md">
+            <Grid.Col span={{ base: 12, md: isConnected ? 4 : 6 }} my="md">
                 <SummaryCard
                     title="Applications"
                     icon={TbApps}
                     value={applications}
                 />
             </Grid.Col>
-            <Grid.Col span={{ base: 12, md: 4 }} my="md">
-                <SummaryCard
-                    title="My Applications"
-                    icon={TbApps}
-                    value={applicationsOwned}
-                />
-            </Grid.Col>
+            {isConnected && (
+                <Grid.Col span={{ base: 12, md: 4 }} my="md">
+                    <SummaryCard
+                        title="My Applications"
+                        icon={TbApps}
+                        value={applicationsOwned}
+                    />
+                </Grid.Col>
+            )}
         </Grid>
     );
 };
