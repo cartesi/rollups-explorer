4ba529b50381efce777a58300d5c85e57fda09c0
dandhee.damarrama <dandhee.damarrama@gmail.com>
2024-03-18 15:32:09 +0700

    feat(apps/web): add pagination on userApplicationsTable

--- apps/web/src/components/applications/applications.tsx
+++ apps/web/src/components/applications/applications.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import { Stack, Title, useMantineColorScheme } from "@mantine/core";
+import {
+    Group,
+    Pagination,
+    Select,
+    Stack,
+    Text,
+    Title,
+    useMantineColorScheme,
+} from "@mantine/core";
 import { FC, useCallback, useState } from "react";
 import { useAccount } from "wagmi";
 import {
@@ -9,7 +17,7 @@ import {
 } from "../../graphql/explorer/hooks/queries";
 import { ApplicationOrderByInput } from "../../graphql/explorer/types";
 import ApplicationsTable from "../applications/applicationsTable";
-import Paginated from "../paginated";
+import Paginated, { perPageList } from "../paginated";
 import UserApplicationsTable from "./userApplicationsTable";
 
 type StackApplicationProps = {
@@ -20,6 +28,7 @@ type StackApplicationProps = {
  *
  * @returns Component to wrap Application or UserApplication coponent
  */
+
 const StackApplication: FC<StackApplicationProps> = ({ children }) => {
     const { colorScheme } = useMantineColorScheme();
     return (
@@ -40,21 +49,43 @@ const StackApplication: FC<StackApplicationProps> = ({ children }) => {
 
 const UserApplications: FC = () => {
     const { address, isConnected } = useAccount();
+    const [page, setPage] = useState(1);
+    const [limit, setLimit] = useState<number>(10);
+    const after = page === 1 ? undefined : ((page - 1) * limit).toString();
     const [query] = useApplicationsConnectionOwnerQuery({
         variables: {
+            after,
+            limit,
             orderBy: ApplicationOrderByInput.IdAsc,
-            ownerId: address?.toLowerCase(),
+            ownerId: "0xf05d57a5bed2d1b529c56001fc5810cc9afc0335".toLowerCase(),
         },
         pause: !isConnected,
     });
+    const totalCount = query.data?.applicationsConnection.totalCount;
+    const totalPages = Math.ceil(
+        totalCount === undefined || totalCount === 0 ? 1 : totalCount / 10,
+    );
     const applications =
         query.data?.applicationsConnection.edges.map((edge) => edge.node) ?? [];
+    const activePage = page > totalPages ? totalPages : page;
+    const changeLimit = useCallback((val: string | null) => {
+        const parsed = val ? parseInt(val, 10) : null;
+        if (typeof parsed === "number" && !isNaN(parsed)) {
+            setLimit(parsed);
+        }
+    }, []);
 
     return (
         <>
             {isConnected && (
                 <StackApplication>
                     <Title order={4}>My Applications</Title>
+                    <Pagination
+                        styles={{ root: { alignSelf: "flex-end" } }}
+                        value={activePage}
+                        total={totalPages}
+                        onChange={setPage}
+                    />
                     <UserApplicationsTable
                         applications={applications}
                         fetching={query.fetching}
@@ -62,6 +93,25 @@ const UserApplications: FC = () => {
                             query.data?.applicationsConnection.totalCount ?? 0
                         }
                     />
+                    <Group justify="space-between" align="center">
+                        <Group>
+                            <Text>Show:</Text>
+                            <Select
+                                style={{ width: "5rem" }}
+                                value={limit.toString()}
+                                onChange={changeLimit}
+                                data={perPageList}
+                            />
+                            <Text>items</Text>
+                        </Group>
+
+                        <Pagination
+                            styles={{ root: { alignSelf: "flex-end" } }}
+                            value={activePage}
+                            total={totalPages}
+                            onChange={setPage}
+                        />
+                    </Group>
                 </StackApplication>
             )}
         </>
