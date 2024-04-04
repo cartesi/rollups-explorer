17cc2f63317f66af999ce716d72c19d4a3047a53
Neven Dyulgerov <neven.diulgerov@gmail.com>
2024-04-04 17:25:04 +0300

    chore(apps/workshop): Upgrade storybook packages

--- apps/workshop/.storybook/main.ts
+++ apps/workshop/.storybook/main.ts
@@ -1,19 +1,25 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-    framework: "@storybook/react-vite",
+    framework: getAbsolutePath("@storybook/react-vite"),
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
     addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/addon-onboarding",
-        "@storybook/addon-interactions",
-        "@storybook/addon-styling",
-        "storybook-dark-mode",
-        "@storybook/addon-viewport",
+        getAbsolutePath("@storybook/addon-links"),
+        getAbsolutePath("@storybook/addon-essentials"),
+        getAbsolutePath("@storybook/addon-onboarding"),
+        getAbsolutePath("@storybook/addon-interactions"),
+        getAbsolutePath("@storybook/addon-themes"),
+        getAbsolutePath("storybook-dark-mode"),
+        getAbsolutePath("@storybook/addon-viewport"),
+        getAbsolutePath("@storybook/addon-mdx-gfm"),
     ],
     docs: {
         autodocs: "tag",
     },
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+    return dirname(require.resolve(join(value, "package.json")));
+}
