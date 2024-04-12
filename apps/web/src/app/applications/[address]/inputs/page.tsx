d597b5802d3fb44c2445e45a0bc2720cf38c3e07
Neven Dyulgerov <neven.diulgerov@gmail.com>
2024-04-12 11:55:19 +0300

    feat(apps/web): Add file

---
+++ apps/web/src/app/applications/[address]/inputs/page.tsx
@@ -0,0 +1,75 @@
+import { Group, Stack, Title } from "@mantine/core";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { FC } from "react";
+import { TbInbox } from "react-icons/tb";
+import { Address as AddressType } from "viem";
+import Address from "../../../components/address";
+import Breadcrumbs from "../../../components/breadcrumbs";
+import Inputs from "../../../components/inputs/inputs";
+import {
+    ApplicationByIdDocument,
+    ApplicationByIdQuery,
+    ApplicationByIdQueryVariables,
+} from "../../../graphql/explorer/operations";
+import { getUrqlServerClient } from "../../../lib/urql";
+
+export async function generateMetadata({
+    params,
+}: ApplicationPageProps): Promise<Metadata> {
+    return {
+        title: `Application ${params.address}`,
+    };
+}
+
+async function getApplication(address: string) {
+    const client = getUrqlServerClient();
+    const result = await client.query<
+        ApplicationByIdQuery,
+        ApplicationByIdQueryVariables
+    >(ApplicationByIdDocument, {
+        id: address.toLowerCase(),
+    });
+
+    return result.data?.applicationById;
+}
+
+export type ApplicationPageProps = {
+    params: { address: string };
+};
+
+const ApplicationPage: FC<ApplicationPageProps> = async ({ params }) => {
+    const application = await getApplication(params.address);
+
+    if (!application) {
+        notFound();
+    }
+
+    return (
+        <Stack>
+            <Breadcrumbs
+                breadcrumbs={[
+                    {
+                        href: "/",
+                        label: "Home",
+                    },
+                    {
+                        href: "/applications",
+                        label: "Applications",
+                    },
+                ]}
+            >
+                <Address value={params.address as AddressType} icon />
+            </Breadcrumbs>
+
+            <Group>
+                <TbInbox size={40} />
+                <Title order={2}>Inputs</Title>
+            </Group>
+
+            <Inputs applicationId={params.address} />
+        </Stack>
+    );
+};
+
+export default ApplicationPage;
