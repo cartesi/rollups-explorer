cc34773943ff6c9f9917a02bd0519ab43cd3e8d7
Bruno Menezes <brunodmenezes@gmail.com>
2024-04-24 13:55:39 +1200

    refactor(e2e): Separate test by device group using the grep/grepInvert configs.

--- apps/web/e2e/layout.spec.ts
+++ apps/web/e2e/layout.spec.ts
@@ -1,10 +1,7 @@
 import { expect, test } from "@playwright/test";
-import e2eConfig from "./config";
-
-const { baseURL } = e2eConfig;
 
 test.beforeEach(async ({ page }) => {
-    await page.goto(baseURL);
+    await page.goto("/");
 });
 
 test("should have page title", async ({ page }) => {
@@ -21,34 +18,18 @@ test("should have a left menu with links", async ({ page }) => {
 
 test("should have a visible and disabled send transaction", async ({
     page,
-    isMobile,
 }) => {
-    let sendTransaction;
-    if (isMobile) {
-        const burgerBtn = page.getByTestId("burger-menu-btn");
-        await expect(burgerBtn).toBeVisible();
-        await burgerBtn.click();
-        sendTransaction = await page.getByTestId("menu-item-send-transaction");
-        await expect(sendTransaction).toHaveAttribute("data-disabled", "true");
-    } else {
-        sendTransaction = await page.getByRole("button", {
-            name: "Send Transaction",
-        });
-        await expect(sendTransaction).toBeDisabled();
-    }
+    const sendTransaction = await page.getByRole("button", {
+        name: "Send Transaction",
+    });
+    await expect(sendTransaction).toBeDisabled();
 
     await expect(sendTransaction).toBeVisible();
 });
 
 test("should have an enabled button to connect to a wallet", async ({
     page,
-    isMobile,
 }) => {
-    if (isMobile) {
-        const burgerBtn = page.getByTestId("burger-menu-btn");
-        await expect(burgerBtn).toBeVisible();
-        await burgerBtn.click();
-    }
     const connectWallet = page.getByRole("button", {
         name: "Connect Wallet",
     });
@@ -65,4 +46,35 @@ test("should have a switch for dark and light mode", async ({ page }) => {
     switchBtn.click();
 
     await expect(switchBtn).toHaveText("Dark Mode");
+});
+
+test.describe("mobile", () => {
+    test("should have a visible and disabled send transaction", async ({
+        page,
+    }) => {
+        const burgerBtn = page.getByTestId("burger-menu-btn");
+        await expect(burgerBtn).toBeVisible();
+        await burgerBtn.click();
+        const sendTransaction = await page.getByTestId(
+            "menu-item-send-transaction",
+        );
+        await expect(sendTransaction).toHaveAttribute("data-disabled", "true");
+
+        await expect(sendTransaction).toBeVisible();
+    });
+
+    test("should have an enabled button to connect to a wallet", async ({
+        page,
+    }) => {
+        const burgerBtn = page.getByTestId("burger-menu-btn");
+        await expect(burgerBtn).toBeVisible();
+        await burgerBtn.click();
+
+        const connectWallet = page.getByRole("button", {
+            name: "Connect Wallet",
+        });
+
+        await expect(connectWallet).toBeVisible();
+        await expect(connectWallet).toBeEnabled();
+    });
 });
--- apps/web/e2e/menu.spec.ts
+++ apps/web/e2e/menu.spec.ts
@@ -1,22 +1,13 @@
 import { expect, test } from "@playwright/test";
-import e2eConfig from "./config";
-
-const { baseURL } = e2eConfig;
 
 test.beforeEach(async ({ page }) => {
-    await page.goto(baseURL);
+    await page.goto("/");
 });
 
 test.describe("Navigations", () => {
-    test("can navigate to applications page", async ({ page, isMobile }) => {
-        if (isMobile) {
-            const burgerBtn = page.getByTestId("burger-menu-btn");
-            await expect(burgerBtn).toBeVisible();
-            await burgerBtn.click();
-        }
-
+    test("can navigate to applications page", async ({ page }) => {
         await page.getByTestId("applications-link").click();
-        await page.waitForURL(`${baseURL}/applications`);
+        await page.waitForURL(`/applications`);
 
         await expect(
             page.getByRole("heading", { name: "Applications" }),
@@ -29,15 +20,9 @@ test.describe("Navigations", () => {
         await expect(page.getByRole("row")).toHaveCount(11);
     });
 
-    test("can navigate to inputs page", async ({ page, isMobile }) => {
-        if (isMobile) {
-            const burgerBtn = page.getByTestId("burger-menu-btn");
-            await expect(burgerBtn).toBeVisible();
-            await burgerBtn.click();
-        }
-
+    test("can navigate to inputs page", async ({ page }) => {
         await page.getByTestId("inputs-link").click();
-        await page.waitForURL(`${baseURL}/inputs`);
+        await page.waitForURL(`/inputs`);
 
         await expect(
             page.getByPlaceholder("Search by Address / Txn Hash / Index"),
@@ -53,5 +38,52 @@ test.describe("Navigations", () => {
 
         // For each input row it actually inserts 3 <tr>
         await expect(page.getByRole("row")).toHaveCount(31);
+    });
+
+    test.describe("mobile", () => {
+        test("can navigate to applications page", async ({ page }) => {
+            const burgerBtn = page.getByTestId("burger-menu-btn");
+            await expect(burgerBtn).toBeVisible();
+            await burgerBtn.click();
+
+            await page.getByTestId("applications-link").click();
+            await page.waitForURL(`/applications`);
+
+            await expect(
+                page.getByRole("heading", { name: "Applications" }),
+            ).toBeVisible();
+
+            await expect(
+                page.getByRole("row", { name: "Id Owner URL" }),
+            ).toBeVisible();
+
+            await expect(page.getByRole("row")).toHaveCount(11);
+        });
+
+        test("can navigate to inputs page", async ({ page }) => {
+            const burgerBtn = page.getByTestId("burger-menu-btn");
+            await expect(burgerBtn).toBeVisible();
+            await burgerBtn.click();
+
+            await page.getByTestId("inputs-link").click();
+            await page.waitForURL(`/inputs`);
+
+            await expect(
+                page.getByPlaceholder("Search by Address / Txn Hash / Index"),
+            ).toBeVisible();
+
+            await expect(
+                page.getByRole("heading", { name: "Inputs" }),
+            ).toBeVisible();
+
+            await expect(
+                page.getByRole("row", {
+                    name: "From To Method Index Age Data",
+                }),
+            ).toBeVisible();
+
+            // For each input row it actually inserts 3 <tr>
+            await expect(page.getByRole("row")).toHaveCount(31);
+        });
     });
 });
