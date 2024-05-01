5ff55201ff4f376581335e6bd55a4182faea16c6
Neven Dyulgerov <neven.diulgerov@gmail.com>
2024-05-01 13:03:04 +0300

    feat(packages/ui): Extend Summary component to support Grid props

--- packages/ui/src/Summary.tsx
+++ packages/ui/src/Summary.tsx
@@ -1,24 +1,25 @@
 "use client";
-import { Grid } from "@mantine/core";
+import { Grid, GridProps } from "@mantine/core";
 import { FC } from "react";
 import { TbApps, TbInbox } from "react-icons/tb";
 import { useAccount } from "wagmi";
 import { SummaryCard } from "./SummaryCard";
 
-export type SummaryProps = {
+export interface SummaryProps extends GridProps {
     inputs: number;
     applications: number;
     applicationsOwned: number;
-};
+}
 
 export const Summary: FC<SummaryProps> = ({
     inputs,
     applications,
     applicationsOwned,
+    ...restProps
 }: SummaryProps) => {
     const { isConnected } = useAccount();
     return (
-        <Grid gutter="md">
+        <Grid gutter="md" {...restProps}>
             <Grid.Col span={{ base: 12, md: isConnected ? 4 : 6 }} my="md">
                 <SummaryCard title="Inputs" icon={TbInbox} value={inputs} />
             </Grid.Col>
