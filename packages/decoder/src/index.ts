94173ae85daf7db8aa3d274037419dac081e3a99
dandhee.damarrama <dandhee.damarrama@gmail.com>
2024-05-07 05:37:41 +0700

    test(packages/decoder) : add unit test

--- packages/decoder/src/index.ts
+++ packages/decoder/src/index.ts
@@ -1,38 +1,54 @@
 import { whatsabi } from "@shazow/whatsabi";
 import { decodeFunctionData } from "viem";
-import abisJson from "./data/abis.json";
 import { provider } from "./utils/provider";
 
 /**
  * Asynchronously decodes a voucher payload using the provided ABI.
  *
- * @param payload - The payload to decode, expected to be a string prefixed with '0x'.
+ * @param destination - The destination address to be executed, expected to be a string prefixed with '0x'.
+ *  @param params - The payload to decode, expected to be a string prefixed with '0x'.
  * @returns The decoded payload or null if an error occurs.
  */
-const decodeVoucherPayload = async (
-    payload: `0x${string}`,
-): Promise<unknown | null> => {
-    try {
-        // Decode the function data to get the voucher payload
-        const { args: voucherPayload } = decodeFunctionData({
-            abi: abisJson,
-            data: payload,
-        });
 
-        // Fetch the ABI using the address from the voucher payload
-        const fetchedAbi = await whatsabi.autoload(
-            voucherPayload![0] as string,
+export type DecodeVoucherPayloadParamsType = {
+    destination: `0x${string}`;
+    payload: `0x${string}`;
+};
+
+export type DecodeVoucherPayloadReturnType =
+    | {
+          functionName: string;
+          args: unknown[] | undefined;
+      }
+    | `0x${string}`;
+
+export const decodeVoucherPayload = async ({
+    destination,
+    payload,
+}: DecodeVoucherPayloadParamsType): Promise<DecodeVoucherPayloadReturnType> => {
+    try {
+        // Fetch the ABI using the destination address f
+        const destinationAddressAbi = await whatsabi.autoload(
+            destination as string,
             { provider },
         );
 
         // Decode the payload using the fetched ABI
-        const { args: decodedPayload } = decodeFunctionData({
-            abi: fetchedAbi.abi,
-            data: voucherPayload![1] as `0x${string}`,
+        const res = decodeFunctionData({
+            abi: destinationAddressAbi.abi,
+            data: payload as `0x${string}`,
         });
-        return decodedPayload;
+
+        console.log({
+            functionName: res.functionName,
+            args: res.args,
+        });
+        return {
+            functionName: res.functionName,
+            args: res.args as [],
+        };
     } catch (error) {
         console.error(error);
-        return null;
+        return payload;
     }
 };
