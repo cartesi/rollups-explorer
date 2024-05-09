8a2d29846a98fd9b6914a47b65cb797293ad96f8
Neven Dyulgerov <neven.diulgerov@gmail.com>
2024-05-09 16:07:48 +0300

    feat(apps/web): Make applications and user applications tables responsive

--- apps/web/src/components/applications/userApplicationsRow.tsx
+++ apps/web/src/components/applications/userApplicationsRow.tsx
@@ -1,4 +1,12 @@
-import { ActionIcon, Box, Group, Table, Text, Tooltip } from "@mantine/core";
+import {
+    ActionIcon,
+    Box,
+    Group,
+    Paper,
+    Table,
+    Text,
+    Tooltip,
+} from "@mantine/core";
 import Link from "next/link";
 import prettyMilliseconds from "pretty-ms";
 import { FC } from "react";
@@ -13,8 +21,12 @@ import { useConnectionConfig } from "../../providers/connectionConfig/hooks";
 import Address from "../address";
 import { ApplicationRowProps } from "./applicationRow";
 
-const UserApplicationsRow: FC<ApplicationRowProps> = (props) => {
-    const { application } = props;
+export interface UserApplicationsRowProps extends ApplicationRowProps {
+    timeType: "timestamp" | "age";
+}
+
+const UserApplicationsRow: FC<UserApplicationsRowProps> = (props) => {
+    const { application, keepDataColVisible, timeType } = props;
     const {
         getConnection,
         hasConnection,
@@ -26,71 +38,117 @@ const UserApplicationsRow: FC<ApplicationRowProps> = (props) => {
     return (
         <Table.Tr key={application.id}>
             <Table.Td>
-                <Address value={appId} icon shorten />
+                <Box
+                    display="flex"
+                    w="max-content"
+                    style={{
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
+                >
+                    <Address value={appId} icon shorten />
+                </Box>
             </Table.Td>
             <Table.Td>{connection?.url ?? "N/A"}</Table.Td>
             <Table.Td>
-                <Box>
+                <Box
+                    display="flex"
+                    w="max-content"
+                    style={{
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
+                >
                     <Text>
-                        {application &&
-                            application?.timestamp &&
-                            `${prettyMilliseconds(
-                                Date.now() - application?.timestamp * 1000,
-                                {
-                                    unitCount: 2,
-                                    secondsDecimalDigits: 0,
-                                    verbose: true,
-                                },
-                            )} ago`}
+                        {application && application?.timestamp && (
+                            <>
+                                {timeType === "age"
+                                    ? `${prettyMilliseconds(
+                                          Date.now() -
+                                              application.timestamp * 1000,
+                                          {
+                                              unitCount: 2,
+                                              secondsDecimalDigits: 0,
+                                              verbose: true,
+                                          },
+                                      )} ago`
+                                    : new Date(
+                                          application.timestamp * 1000,
+                                      ).toISOString()}
+                            </>
+                        )}
                     </Text>
                 </Box>
             </Table.Td>
-            <Table.Td>
-                <Group gap="xs">
-                    <Tooltip label="Summary">
-                        <Link
-                            href={`/applications/${appId}`}
-                            data-testid="applications-summary-link"
-                        >
-                            <ActionIcon variant="default">
-                                <TbStack2 />
-                            </ActionIcon>
-                        </Link>
-                    </Tooltip>
-                    <Tooltip label="Inputs">
-                        <Link
-                            href={`/applications/${appId}/inputs`}
-                            data-testid="applications-link"
-                        >
-                            <ActionIcon variant="default">
-                                <TbInbox />
-                            </ActionIcon>
-                        </Link>
-                    </Tooltip>
-                    {hasConnection(appId) ? (
-                        <Tooltip label="Remove connection">
-                            <ActionIcon
-                                data-testid="remove-connection"
-                                variant="default"
-                                ml={4}
-                                onClick={() => removeConnection(appId)}
-                            >
-                                <TbPlugConnectedX />
-                            </ActionIcon>
-                        </Tooltip>
-                    ) : (
-                        <Tooltip label="Add a connection">
-                            <ActionIcon
-                                data-testid="add-connection"
-                                variant="default"
-                                ml={4}
-                                onClick={() => showConnectionModal(appId)}
-                            >
-                                <TbPlugConnected />
-                            </ActionIcon>
-                        </Tooltip>
-                    )}
-                </Group>
+
+            <Table.Td
+                pos={keepDataColVisible ? "initial" : "sticky"}
+                top={0}
+                right={0}
+                p={0}
+            >
+                <Paper
+                    shadow={keepDataColVisible ? undefined : "xl"}
+                    radius={0}
+                    p="var(--table-vertical-spacing) var(--table-horizontal-spacing, var(--mantine-spacing-xs))"
+                >
+                    <Box
+                        display="flex"
+                        w="max-content"
+                        style={{
+                            alignItems: "center",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <Group gap="xs">
+                            <Tooltip label="Summary">
+                                <Link
+                                    href={`/applications/${appId}`}
+                                    data-testid="applications-summary-link"
+                                >
+                                    <ActionIcon variant="default">
+                                        <TbStack2 />
+                                    </ActionIcon>
+                                </Link>
+                            </Tooltip>
+                            <Tooltip label="Inputs">
+                                <Link
+                                    href={`/applications/${appId}/inputs`}
+                                    data-testid="applications-link"
+                                >
+                                    <ActionIcon variant="default">
+                                        <TbInbox />
+                                    </ActionIcon>
+                                </Link>
+                            </Tooltip>
+                            {hasConnection(appId) ? (
+                                <Tooltip label="Remove connection">
+                                    <ActionIcon
+                                        data-testid="remove-connection"
+                                        variant="default"
+                                        ml={4}
+                                        onClick={() => removeConnection(appId)}
+                                    >
+                                        <TbPlugConnectedX />
+                                    </ActionIcon>
+                                </Tooltip>
+                            ) : (
+                                <Tooltip label="Add a connection">
+                                    <ActionIcon
+                                        data-testid="add-connection"
+                                        variant="default"
+                                        ml={4}
+                                        onClick={() =>
+                                            showConnectionModal(appId)
+                                        }
+                                    >
+                                        <TbPlugConnected />
+                                    </ActionIcon>
+                                </Tooltip>
+                            )}
+                        </Group>
+                    </Box>
+                </Paper>
             </Table.Td>
         </Table.Tr>
     );
--- apps/web/src/components/applications/userApplicationsTable.tsx
+++ apps/web/src/components/applications/userApplicationsTable.tsx
@@ -1,8 +1,18 @@
-import { Box, Loader, Table, Text } from "@mantine/core";
-import { FC } from "react";
+import {
+    Button,
+    Loader,
+    Table,
+    Text,
+    Transition,
+    useMantineColorScheme,
+    useMantineTheme,
+} from "@mantine/core";
+import { FC, useCallback, useRef, useState } from "react";
 import { Application } from "../../graphql/explorer/types";
 import { ApplicationsTableProps } from "./applicationsTable";
 import UserApplicationsRow from "./userApplicationsRow";
+import { useElementVisibility } from "../../hooks/useElementVisibility";
+import TableResponsiveWrapper from "../tableResponsiveWrapper";
 
 interface UserApplicationsTableProps extends ApplicationsTableProps {
     noResultsMessage?: string;
@@ -15,27 +25,71 @@ const UserApplicationsTable: FC<UserApplicationsTableProps> = (props) => {
         totalCount,
         noResultsMessage = "No applications",
     } = props;
+    const tableRowRef = useRef<HTMLDivElement>(null);
+    const theme = useMantineTheme();
+    const { colorScheme } = useMantineColorScheme();
+    const bgColor = colorScheme === "dark" ? theme.colors.dark[7] : theme.white;
+    const { childrenRef, isVisible } = useElementVisibility({
+        element: tableRowRef,
+    });
+    const [timeType, setTimeType] = useState<"timestamp" | "age">("age");
+
+    const onChangeTimeColumnType = useCallback(() => {
+        setTimeType((timeType) => (timeType === "age" ? "timestamp" : "age"));
+    }, []);
+
     return (
-        <Box>
-            <Table>
+        <TableResponsiveWrapper ref={tableRowRef}>
+            <Table width={"100%"} style={{ borderCollapse: "collapse" }}>
                 <Table.Thead>
                     <Table.Tr>
                         <Table.Th>Id</Table.Th>
                         <Table.Th>URL</Table.Th>
-                        <Table.Th>Age</Table.Th>
+                        <Table.Th>
+                            <Button
+                                variant="transparent"
+                                px={0}
+                                onClick={onChangeTimeColumnType}
+                            >
+                                {timeType === "age" ? "Age" : "Timestamp (UTC)"}
+                            </Button>
+                        </Table.Th>
+                        <Table.Th ref={childrenRef}>Data</Table.Th>
+                        <Transition
+                            mounted={isVisible}
+                            transition="scale-x"
+                            duration={500}
+                            timingFunction="ease-out"
+                        >
+                            {(styles) => (
+                                <th
+                                    style={{
+                                        ...styles,
+                                        position: "sticky",
+                                        top: 0,
+                                        right: 0,
+                                        backgroundColor: bgColor,
+                                        padding:
+                                            "var(--table-vertical-spacing) var(--table-horizontal-spacing, var(--mantine-spacing-lg))",
+                                    }}
+                                >
+                                    Data
+                                </th>
+                            )}
+                        </Transition>
                     </Table.Tr>
                 </Table.Thead>
                 <Table.Tbody>
                     {fetching ? (
                         <Table.Tr>
-                            <Table.Td align="center" colSpan={3}>
+                            <Table.Td align="center" colSpan={4}>
                                 <Loader />
                             </Table.Td>
                         </Table.Tr>
                     ) : (
                         totalCount === 0 && (
                             <Table.Tr>
-                                <Table.Td colSpan={3} align="center">
+                                <Table.Td colSpan={4} align="center">
                                     <Text fw={700}>{noResultsMessage}</Text>
                                 </Table.Td>
                             </Table.Tr>
@@ -47,11 +101,13 @@ const UserApplicationsTable: FC<UserApplicationsTableProps> = (props) => {
                             application={
                                 application as Omit<Application, "inputs">
                             }
+                            timeType={timeType}
+                            keepDataColVisible={!isVisible}
                         />
                     ))}
                 </Table.Tbody>
             </Table>
-        </Box>
+        </TableResponsiveWrapper>
     );
 };
 
