9477186221e7ca5518f0e69d82b0b11cb7b3aa7c
Bruno Menezes <brunodmenezes@gmail.com>
2024-05-19 19:35:24 +1200

    feat: Add decoder for abi_params and json_abi modes, custom errors, types and test cases.

---
+++ apps/web/src/components/specification/decoder.ts
@@ -0,0 +1,192 @@
+import { T, cond, head, includes, pathEq, pathOr, pipe } from "ramda";
+import {
+    AbiFunction,
+    AbiFunctionSignatureNotFoundError,
+    Hex,
+    decodeAbiParameters,
+    decodeFunctionData,
+    parseAbiParameters,
+    slice,
+} from "viem";
+import SpecificationModeNotSupportedError from "./errors/SpecificationModeNotSupported";
+import { ABI_PARAMS, JSON_ABI, Specification, specModes } from "./types";
+
+interface Piece {
+    name: string;
+    part: Hex | Uint8Array;
+    decodedPart?: any;
+}
+
+interface Envelope {
+    spec: Specification;
+    input: Hex | Uint8Array;
+    pieces: Piece[];
+    result: Record<string, any>;
+    error?: Error;
+}
+
+const getPieces = (
+    abiParams: readonly string[],
+    encodedData: Hex | Uint8Array,
+): Piece[] => {
+    const abiParameters = parseAbiParameters(abiParams);
+
+    const resultList = decodeAbiParameters(
+        // @ts-ignore dynamic type complains
+        abiParameters,
+        encodedData,
+    );
+
+    return resultList.map((decodedVal, index) => {
+        // @ts-ignore
+        const { name } = abiParameters[index] as {
+            name: string;
+        };
+
+        return {
+            name,
+            part: encodedData,
+            decodedPart: decodedVal,
+        } as Piece;
+    });
+};
+
+const addPiecesToEnvelope = (e: Envelope): Envelope => {
+    if (e.spec.mode === "abi_params") {
+        try {
+            if (e.spec.sliceInstructions?.length) {
+                e.spec.sliceInstructions.forEach((instruction, index) => {
+                    const { from, to, name, type } = instruction;
+                    const part = slice(e.input, from, to);
+                    const decodedPart =
+                        type !== undefined
+                            ? head(decodeAbiParameters([{ type, name }], part))
+                            : part;
+
+                    e.pieces.push({
+                        name: name ?? `param${index}`,
+                        part,
+                        decodedPart,
+                    });
+                });
+            } else {
+                const pieces = getPieces(e.spec.abiParams, e.input);
+                e.pieces.push(...pieces);
+            }
+        } catch (error: any) {
+            const message = pathOr(error.message, ["shortMessage"], error);
+            const errorMeta = pathOr([], ["metaMessages"], error).join("\n");
+            e.error = new Error(`${message}\n\n${errorMeta}`);
+        }
+    }
+
+    return e;
+};
+
+const decodeTargetSliceAndAddToPieces = (e: Envelope): Envelope => {
+    if (e.spec.mode === "abi_params") {
+        const targetName = e.spec.sliceTarget;
+        const piece = e.pieces.find((piece) => piece.name === targetName);
+
+        if (piece && piece.part) {
+            const pieces = getPieces(e.spec.abiParams, piece.part);
+            e.pieces.push(...pieces);
+        }
+    }
+    return e;
+};
+
+const prepareResultFromPieces = (e: Envelope): Envelope => {
+    if (e.spec.mode === "abi_params") {
+        const sliceTarget = e.spec.sliceTarget;
+        e.result = e.pieces.reduce((prev, { name, decodedPart }, index) => {
+            /**
+             * Adding a unwrap effect
+             * decoded target is not included in the result
+             */
+            if (sliceTarget === name) return prev;
+
+            const key = name ?? `params${index}`;
+            return { ...prev, [key]: decodedPart };
+        }, {});
+    }
+
+    return e;
+};
+
+const prepareResultForJSONABI = (e: Envelope): Envelope => {
+    if (e.spec.mode === "json_abi") {
+        try {
+            const { functionName, args } = decodeFunctionData({
+                abi: e.spec.abi,
+                data: e.input as Hex,
+            });
+
+            const orderedNamedArgs: [string, any][] = [];
+
+            if (args && args?.length > 0) {
+                const abiItem = e.spec.abi.find(
+                    (item) =>
+                        item.type === "function" && item.name === functionName,
+                ) as AbiFunction;
+
+                // respecting order of arguments but including abi names
+                abiItem.inputs.forEach((param, index) => {
+                    const name = param.name ?? `param${0}`;
+                    orderedNamedArgs.push([name, args[index]]);
+                });
+            }
+
+            e.result = {
+                functionName,
+                args,
+                orderedNamedArgs,
+            };
+        } catch (err: any) {
+            const message =
+                err instanceof AbiFunctionSignatureNotFoundError
+                    ? err.shortMessage
+                    : err.message;
+            e.error = new Error(message);
+        }
+    }
+
+    return e;
+};
+
+const transform: (e: Envelope) => Envelope = cond([
+    [
+        pathEq(ABI_PARAMS, ["spec", "mode"]),
+        pipe(
+            addPiecesToEnvelope,
+            decodeTargetSliceAndAddToPieces,
+            prepareResultFromPieces,
+        ),
+    ],
+    [pathEq(JSON_ABI, ["spec", "mode"]), prepareResultForJSONABI],
+    [
+        T,
+        (e: Envelope) => {
+            e.error = new SpecificationModeNotSupportedError(e.spec);
+            return e;
+        },
+    ],
+]);
+
+export function decodeInputPayload(
+    spec: Specification,
+    input: Hex | Uint8Array,
+): Envelope {
+    if (!includes(spec.mode, specModes)) {
+        throw new SpecificationModeNotSupportedError(spec);
+    }
+
+    const envelope: Envelope = {
+        spec,
+        input,
+        pieces: [],
+        result: {},
+    };
+
+    return transform(envelope);
+}
---
+++ apps/web/src/components/specification/types.ts
@@ -0,0 +1,59 @@
+import { AbiType } from "abitype";
+import { Abi } from "viem";
+
+export const JSON_ABI = "json_abi" as const;
+export const ABI_PARAMS = "abi_params" as const;
+export const specModes = [JSON_ABI, ABI_PARAMS] as const;
+export const equalityOperators = ["equals"] as const;
+export const logicalOperators = ["and", "or"] as const;
+export const inputProperties = ["application.id", "msgSender"] as const;
+
+export type Modes = (typeof specModes)[number];
+export type Operator = (typeof equalityOperators)[number];
+export type ConditionalOperator = (typeof logicalOperators)[number];
+export type FieldPath = (typeof inputProperties)[number];
+
+export interface Condition {
+    operator: Operator;
+    field: FieldPath;
+    value: string;
+}
+
+export interface Predicate {
+    logicalOperator: ConditionalOperator;
+    conditions: Condition[];
+}
+
+interface SliceInstruction {
+    /** Start index of the hex or byte-array*/
+    from: number;
+    /** End index of the hex or byte-array. Undefined means getting from start onwards e.g. arbitrary data size.*/
+    to?: number;
+    /** A name for the final decoded object or for reference when the slice is a target for abi-params decoding*/
+    name?: string;
+    /** The type to decode e.g. uint. Leaving empty will default to the raw sliced return; useful for Address for example.*/
+    type?: AbiType;
+}
+
+type Commons = {
+    name: string;
+    timestamp?: number;
+    conditionals?: Predicate[];
+};
+
+export type AbiParamsSpec = Commons & {
+    mode: "abi_params";
+    abiParams: readonly string[];
+    /** Optional: instructions to treat the encoded data before applying the abiParams definition to a specific named part.*/
+    sliceInstructions?: SliceInstruction[];
+    /** Optional: find a named sliced data and decode it applying the defined values set on abiParams property. */
+    sliceTarget?: string;
+};
+
+export type JSONAbiSpec = Commons & {
+    mode: "json_abi";
+    /** Full fledge-json-abi to decode encoded data with 4 bytes selector + any arguments it has. */
+    abi: Abi;
+};
+
+export type Specification = AbiParamsSpec | JSONAbiSpec;
