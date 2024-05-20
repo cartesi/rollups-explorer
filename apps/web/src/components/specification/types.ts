df3f30167532bed5e7c26aceedaf064574af5808
Bruno Menezes <brunodmenezes@gmail.com>
2024-05-20 22:58:40 +1200

    feat: Add conditionals to match against a input and return a Specification.

--- apps/web/src/components/specification/types.ts
+++ apps/web/src/components/specification/types.ts
@@ -4,12 +4,12 @@ import { Abi } from "viem";
 export const JSON_ABI = "json_abi" as const;
 export const ABI_PARAMS = "abi_params" as const;
 export const specModes = [JSON_ABI, ABI_PARAMS] as const;
-export const equalityOperators = ["equals"] as const;
+export const operators = ["equals"] as const;
 export const logicalOperators = ["and", "or"] as const;
 export const inputProperties = ["application.id", "msgSender"] as const;
 
 export type Modes = (typeof specModes)[number];
-export type Operator = (typeof equalityOperators)[number];
+export type Operator = (typeof operators)[number];
 export type ConditionalOperator = (typeof logicalOperators)[number];
 export type FieldPath = (typeof inputProperties)[number];
 
@@ -39,21 +39,23 @@ type Commons = {
     name: string;
     timestamp?: number;
     conditionals?: Predicate[];
+    /** Reference to the implementation version. Not controlled by user.*/
+    version?: number;
 };
-
-export type AbiParamsSpec = Commons & {
+export interface AbiParamsSpecification extends Commons {
     mode: "abi_params";
+    /** List of human readable ABI format entries*/
     abiParams: readonly string[];
     /** Optional: instructions to treat the encoded data before applying the abiParams definition to a specific named part.*/
     sliceInstructions?: SliceInstruction[];
     /** Optional: find a named sliced data and decode it applying the defined values set on abiParams property. */
     sliceTarget?: string;
-};
+}
 
-export type JSONAbiSpec = Commons & {
+export interface JSONAbiSpecification extends Commons {
     mode: "json_abi";
     /** Full fledge-json-abi to decode encoded data with 4 bytes selector + any arguments it has. */
     abi: Abi;
-};
+}
 
-export type Specification = AbiParamsSpec | JSONAbiSpec;
+export type Specification = AbiParamsSpecification | JSONAbiSpecification;
