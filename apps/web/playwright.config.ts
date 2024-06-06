5421c4b15d9f39bf637f5eefb0d005ec81bccd98
Neven Dyulgerov <neven.diulgerov@gmail.com>
2024-06-06 12:45:46 +0300

    feat(apps/web): Add e2e tests to the build

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -53,9 +53,10 @@ export default defineConfig({
     ],
 
     /* Run your local dev server before starting the tests */
-    // webServer: {
-    //     command: "yarn run start",
-    //     url: "http://127.0.0.1:3000",
-    //     reuseExistingServer: !process.env.CI,
-    // },
+    webServer: {
+        command: "yarn run dev",
+        url: "http://127.0.0.1:3000",
+        reuseExistingServer: !process.env.CI,
+        timeout: 120 * 1000,
+    },
 });
