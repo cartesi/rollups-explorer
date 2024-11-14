c9a0f203daacfa101253f048d267371c20a9aa3f
Bruno Menezes <brunodmenezes@gmail.com>
2024-11-14 13:59:03 +1300

    feat: Add new system specifications for v2. Add new test cases + data and update a failing one.

--- apps/web/src/components/specification/systemSpecs.ts
+++ apps/web/src/components/specification/systemSpecs.ts
@@ -9,6 +9,11 @@ import {
     erc20PortalConfig,
     erc721PortalConfig,
     etherPortalConfig,
+    v2Erc1155BatchPortalConfig,
+    v2Erc1155SinglePortalConfig,
+    v2Erc20PortalConfig,
+    v2Erc721PortalConfig,
+    v2EtherPortalConfig,
 } from "@cartesi/rollups-wagmi";
 import { Specification } from "./types";
 
@@ -174,6 +179,138 @@ const DAppAddressRelaySpec: Specification = {
     ],
 };
 
+const V2EtherPortalSpec: Specification = {
+    id: "52a35267-7c06-4f0b-b329-0f567eab5ed4",
+    version: 1,
+    mode: "abi_params",
+    name: "Ether Portal @cartesi/rollups@2.x",
+    sliceInstructions: EtherPortalSpec.sliceInstructions,
+    abiParams: [],
+    conditionals: [
+        {
+            logicalOperator: "or",
+            conditions: [
+                {
+                    field: "msgSender",
+                    operator: "equals",
+                    value: v2EtherPortalConfig.address.toLowerCase(),
+                },
+            ],
+        },
+    ],
+} as const;
+
+const V2ERC1155SinglePortalSpec: Specification = {
+    id: "68ae9335-2494-437b-87c7-ac0c8dc7058b",
+    mode: "abi_params",
+    name: `ERC-1155 Single Portal @cartesi/rollups@2.x`,
+    sliceInstructions: ERC1155SinglePortalSpec.sliceInstructions,
+    conditionals: [
+        {
+            logicalOperator: "or",
+            conditions: [
+                {
+                    field: "msgSender",
+                    operator: "equals",
+                    value: v2Erc1155SinglePortalConfig.address.toLowerCase(),
+                },
+            ],
+        },
+    ],
+    abiParams: [],
+    version: 1,
+} as const;
+
+const V2ERC1155BatchPortalSpec: Specification = {
+    id: "54ee4a79-f725-4078-98ac-d2fa5fd287f9",
+    mode: "abi_params",
+    name: "ERC-1155 Batch Portal @cartesi/rollups@2.x",
+    abiParams: [
+        "uint[] tokenIds, uint[] amounts, bytes baseLayer, bytes execLayer",
+    ],
+    sliceInstructions: [
+        {
+            from: 0,
+            to: 20,
+            name: "tokenAddress",
+        },
+        {
+            from: 20,
+            to: 40,
+            name: "from",
+        },
+        {
+            from: 40,
+            name: "data",
+        },
+    ],
+    sliceTarget: "data",
+    conditionals: [
+        {
+            logicalOperator: "or",
+            conditions: [
+                {
+                    field: "msgSender",
+                    operator: "equals",
+                    value: v2Erc1155BatchPortalConfig.address.toLowerCase(),
+                },
+            ],
+        },
+    ],
+    version: 1,
+} as const;
+
+const V2ERC20PortalSpec: Specification = {
+    id: "451150d0-6936-4e4a-882d-44b264bf5f30",
+    mode: "abi_params",
+    name: "ERC-20 Portal @cartesi/rollups@2.x",
+    sliceInstructions: [
+        { from: 0, to: 20, name: "tokenAddress" },
+        { from: 20, to: 40, name: "from" },
+        { from: 40, to: 72, name: "amount", type: "uint" },
+        { from: 72, name: "execLayerData", optional: true },
+    ],
+    abiParams: [],
+    conditionals: [
+        {
+            logicalOperator: "or",
+            conditions: [
+                {
+                    field: "msgSender",
+                    operator: "equals",
+                    value: v2Erc20PortalConfig.address.toLowerCase(),
+                },
+            ],
+        },
+    ],
+    version: 1,
+} as const;
+
+const V2ERC721PortalSpec: Specification = {
+    id: "12fa0cd4-aaa2-4ec9-b38d-0cb0cb19a919",
+    version: 1,
+    mode: "abi_params",
+    name: "ERC-721 Portal @cartesi/rollups@2.x",
+    sliceInstructions: [
+        { from: 0, to: 20, name: "tokenAddress" },
+        { from: 20, to: 40, name: "from" },
+        { from: 40, to: 72, name: "tokenIndex", type: "uint" },
+    ],
+    abiParams: [],
+    conditionals: [
+        {
+            logicalOperator: "or",
+            conditions: [
+                {
+                    field: "msgSender",
+                    operator: "equals",
+                    value: v2Erc721PortalConfig.address.toLowerCase(),
+                },
+            ],
+        },
+    ],
+} as const;
+
 export const systemSpecification = {
     ERC1155SinglePortalSpec,
     ERC1155BatchPortalSpec,
@@ -181,6 +318,11 @@ export const systemSpecification = {
     ERC721PortalSpec,
     EtherPortalSpec,
     DAppAddressRelaySpec,
+    V2EtherPortalSpec,
+    V2ERC1155BatchPortalSpec,
+    V2ERC1155SinglePortalSpec,
+    V2ERC20PortalSpec,
+    V2ERC721PortalSpec,
 } as const;
 
 export const systemSpecificationAsList = Object.values(systemSpecification);
