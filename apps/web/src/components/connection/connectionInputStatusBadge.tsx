69617b801c691abbbe8647e79321b96889de08e7
Bruno Menezes <brunodmenezes@gmail.com>
2024-11-15 17:39:07 +1300

    feat: Update input-row method-resolver and input-status to support v2 changes.

--- apps/web/src/components/connection/connectionInputStatusBadge.tsx
+++ apps/web/src/components/connection/connectionInputStatusBadge.tsx
@@ -1,45 +1,78 @@
 "use client";
 import { Badge, Loader } from "@mantine/core";
-import React, { FC, useMemo } from "react";
+import { FC, useMemo } from "react";
 import { useQuery } from "urql";
+import { RollupVersion } from "../../graphql/explorer/types";
 import {
     InputStatusDocument,
     InputStatusQuery,
     InputStatusQueryVariables,
 } from "../../graphql/rollups/operations";
+import {
+    InputStatusDocument as V2StatusDocument,
+    InputStatusQuery as V2StatusQuery,
+    InputStatusQueryVariables as V2StatusQueryVariables,
+} from "../../graphql/rollups/v2/operations";
 
 interface ConnectionInputStatusBadgeProps {
     graphqlUrl: string;
     index: number;
+    application: {
+        rollupVersion: RollupVersion;
+        id: string;
+        address: string;
+    };
 }
 
 const ConnectionInputStatusBadge: FC<ConnectionInputStatusBadgeProps> = ({
     graphqlUrl,
     index,
+    application,
 }) => {
+    const context = useMemo(
+        () => ({
+            url: graphqlUrl,
+        }),
+        [graphqlUrl],
+    );
+
     const [result] = useQuery<InputStatusQuery, InputStatusQueryVariables>({
-        context: useMemo(
-            () => ({
-                url: graphqlUrl,
-            }),
-            [graphqlUrl],
-        ),
+        context: context,
         query: InputStatusDocument,
         variables: {
             index,
         },
+        pause: application.rollupVersion !== RollupVersion.V1,
     });
+
+    const [resultV2] = useQuery<V2StatusQuery, V2StatusQueryVariables>({
+        context: context,
+        query: V2StatusDocument,
+        variables: {
+            id: index.toString(),
+        },
+        pause: application.rollupVersion !== RollupVersion.V2,
+    });
+
+    const fetching =
+        application.rollupVersion === RollupVersion.V1
+            ? result.fetching
+            : resultV2.fetching;
+    const data =
+        application.rollupVersion === RollupVersion.V1
+            ? result.data
+            : resultV2.data;
 
     return (
         <>
-            {result.fetching ? (
+            {fetching ? (
                 <Loader
                     data-testid="connection-input-status-loader"
                     size="xs"
                 />
-            ) : result.data?.input.status ? (
+            ) : data?.input.status ? (
                 <Badge variant="default" style={{ textTransform: "none" }}>
-                    {result.data.input.status}
+                    {data.input.status}
                 </Badge>
             ) : (
                 "N/A"
