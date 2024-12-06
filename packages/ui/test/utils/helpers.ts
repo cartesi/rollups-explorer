e40d73712bb5af23f7bc47bf16990a89dd46701f
Bruno Menezes <brunodmenezes@gmail.com>
2024-12-06 22:51:37 +1300

    feat: Add rollup-v2 support for Generic-Input form.

--- packages/ui/test/utils/helpers.ts
+++ packages/ui/test/utils/helpers.ts
@@ -43,29 +43,3 @@ const depositWaitStatus = {
         this._status = "idle";
     },
 };
-
-/**
- * Factory of stateful wait-status object. Using prototype pattern to avoid problems with
- * concurrency between test case that makes use of an instance.
- *
- * @description Return a stateful object to support testing callback calls like onSuccces, onSearchApplication etc.
- * That wait object representation helps to avoid infinite loop by making a computed prop change
- * when e.g. a deposit reset is called.
- * @returns
- */
-export const factoryWaitStatus = () => {
-    return {
-        _status: "success",
-        get props(): {
-            status?: "success";
-            isSuccess?: true;
-        } {
-            return this._status === "success"
-                ? { status: "success", isSuccess: true }
-                : {};
-        },
-        reset() {
-            this._status = "idle";
-        },
-    };
-};
