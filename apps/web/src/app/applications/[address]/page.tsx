b99ec3465e95f4b28c54abf968c34f854c0f545a
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-01-26 17:03:43 +0200

    feat(apps/web): Shorten breadcrumbs application address. Change page title to "Application Summary".

--- apps/web/src/app/applications/[address]/page.tsx
+++ apps/web/src/app/applications/[address]/page.tsx
@@ -65,10 +65,10 @@ const ApplicationPage: FC<ApplicationPageProps> = async (props) => {
                     },
                 ]}
             >
-                <Address value={params.address as AddressType} icon />
+                <Address value={params.address as AddressType} icon shorten />
             </Breadcrumbs>
 
-            <PageTitle title="Summary" Icon={TbStack2} />
+            <PageTitle title="Application Summary" Icon={TbStack2} />
             <ApplicationSummary applicationId={params.address} />
         </Stack>
     );
