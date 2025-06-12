3782d6bd0217b9c21ab43a0cd021148048f23ce9
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-06-12 19:34:12 +0300

    fix: Fix build for domains package

--- packages/domain/tsup.config.ts
+++ packages/domain/tsup.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ["src/index.tsx"],
+    entry: ["src/**/**.{ts,tsx}"],
     format: ["esm", "cjs"],
     dts: true,
+    clean: true,
     sourcemap: true,
     esbuildOptions(options, context) {
         options.banner = {
