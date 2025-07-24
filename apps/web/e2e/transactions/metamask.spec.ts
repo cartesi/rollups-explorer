9b8a3e8f08cf7b9ade1f740a2c8619c0a6ab2516
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-07-24 14:03:09 +0300

    test: (apps/web): Add more metamask e2e tests

--- apps/web/e2e/transactions/metamask.spec.ts
+++ apps/web/e2e/transactions/metamask.spec.ts
@@ -7,23 +7,18 @@ test.describe.configure({
 });
 
 test.describe("Metamask", () => {
-    test.beforeEach(async ({ page }) => {
+    test.beforeEach(async ({ wallet, page }) => {
         await page.goto("/");
-    });
-
-    test("should render 'Ether Deposit' transaction form", async ({
-        wallet,
-        page,
-    }) => {
         const connectButton = page.getByText("Connect Wallet");
         await connectButton.click();
         const metamaskButton = page.getByText("Metamask");
         await metamaskButton.click();
         await wallet.approve();
-
         const sendTransactionButton = page.getByTestId("transaction-button");
         await sendTransactionButton.click();
+    });
 
+    test("should render 'Ether Deposit' transaction form", async ({ page }) => {
         const modal = page.getByRole("dialog");
         await expect(modal).toBeVisible();
 
@@ -49,18 +44,119 @@ test.describe("Metamask", () => {
         ).toBeHidden();
     });
 
-    test("should render 'Raw Input' transaction form", async ({
-        wallet,
+    test("should render 'ERC-20' transaction form", async ({ page }) => {
+        const modal = page.getByRole("dialog");
+        await expect(modal).toBeVisible();
+
+        const selectInput = modal.locator('input[value="Ether Deposit"]');
+        await selectInput.click();
+        await page.getByText("ERC-20 Deposit").click();
+
+        await expect(modal.getByTestId("erc20-deposit-form")).toBeVisible();
+        await expect(
+            modal.getByText("The application smart contract address"),
+        ).toBeVisible();
+
+        const applicationInput = page.getByTestId("application-input");
+        await applicationInput.focus();
+        const option = page.getByRole("option").first();
+        await option.click();
+
+        await expect(
+            modal.getByText("The ERC-20 smart contract address"),
+        ).toBeVisible();
+    });
+
+    test("should render 'ERC-721' transaction form", async ({ page }) => {
+        const modal = page.getByRole("dialog");
+        await expect(modal).toBeVisible();
+
+        const selectInput = modal.locator('input[value="Ether Deposit"]');
+        await selectInput.click();
+        await page.getByText("ERC-721 Deposit").click();
+
+        await expect(modal.getByTestId("erc721-deposit-form")).toBeVisible();
+        await expect(
+            modal.getByText("The application smart contract address"),
+        ).toBeVisible();
+
+        const applicationInput = page.getByTestId("application");
+        await applicationInput.focus();
+        const option = page.getByRole("option").first();
+        await option.click();
+
+        await expect(
+            modal.getByText("The ERC-721 smart contract address"),
+        ).toBeVisible();
+    });
+
+    test("should render 'ERC-1155' transaction form", async ({ page }) => {
+        const modal = page.getByRole("dialog");
+        await expect(modal).toBeVisible();
+
+        const selectInput = modal.locator('input[value="Ether Deposit"]');
+        await selectInput.click();
+        await page.getByText("ERC-1155 Deposit").click();
+
+        await expect(modal.getByTestId("erc1155-deposit-form")).toBeVisible();
+        await expect(
+            modal.getByText("The application smart contract address"),
+        ).toBeVisible();
+
+        const applicationInput = page.getByTestId("application");
+        await applicationInput.focus();
+        const option = page.getByRole("option").first();
+        await option.click();
+
+        await expect(
+            modal.getByText("The ERC-1155 smart contract address"),
+        ).toBeVisible();
+    });
+
+    test("should render 'ERC-1155 Batch' transaction form", async ({
         page,
     }) => {
-        const connectButton = page.getByText("Connect Wallet");
-        await connectButton.click();
-        const metamaskButton = page.getByText("Metamask");
-        await metamaskButton.click();
-        await wallet.approve();
-        const sendTransactionButton = page.getByTestId("transaction-button");
-        await sendTransactionButton.click();
+        const modal = page.getByRole("dialog");
+        await expect(modal).toBeVisible();
 
+        const selectInput = modal.locator('input[value="Ether Deposit"]');
+        await selectInput.click();
+        await page.getByText("ERC-1155 Batch Deposit").click();
+
+        await expect(
+            modal.getByTestId("erc1155-batch-deposit-form"),
+        ).toBeVisible();
+        await expect(
+            modal.getByText("The application smart contract address"),
+        ).toBeVisible();
+
+        const applicationInput = page.getByTestId("application");
+        await applicationInput.focus();
+        const option = page.getByRole("option").first();
+        await option.click();
+
+        await expect(
+            modal.getByText("The ERC-1155 smart contract address"),
+        ).toBeVisible();
+    });
+
+    test("should render 'Application Address' transaction form", async ({
+        page,
+    }) => {
+        const modal = page.getByRole("dialog");
+        await expect(modal).toBeVisible();
+
+        const selectInput = modal.locator('input[value="Ether Deposit"]');
+        await selectInput.click();
+        await page.getByText("Application Address").click();
+
+        await expect(modal.getByTestId("address-relay-form")).toBeVisible();
+        await expect(
+            modal.getByText("The application address to relay."),
+        ).toBeVisible();
+    });
+
+    test("should render 'Generic Input' transaction form", async ({ page }) => {
         const modal = page.getByRole("dialog");
         await expect(modal).toBeVisible();
 
