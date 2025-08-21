3bad16f0cbdaf4c782ccced29adc9d535c6a50b3
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-08-21 09:58:38 +0300

    fix (packages/ui): Verify that the query key or block number are different from the last ones before invalidating the query

--- packages/ui/src/hooks/useWatchQueryOnBlockChange.ts
+++ packages/ui/src/hooks/useWatchQueryOnBlockChange.ts
@@ -1,14 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import { useBlockNumber } from "wagmi";
+import { isEqual } from "../utils";
 
 export default function useWatchQueryOnBlockChange(
     queryKey: readonly unknown[],
 ) {
     const queryClient = useQueryClient();
     const { data: blockNumber } = useBlockNumber({ watch: true });
+    const lastQueryKey = useRef(queryKey);
+    const lastBlockNumber = useRef(blockNumber);
 
     useEffect(() => {
-        queryClient.invalidateQueries({ queryKey });
+        if (
+            !isEqual(queryKey, lastQueryKey.current) ||
+            blockNumber !== lastBlockNumber.current
+        ) {
+            lastQueryKey.current = queryKey;
+            lastBlockNumber.current = blockNumber;
+            void queryClient.invalidateQueries({ queryKey });
+        }
     }, [blockNumber, queryClient, queryKey]);
 }
