bdbc9365f74bda03a6bf089a674b912f7b98b7e9
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-08-27 20:38:51 +0300

    feat (packages/ui): Simplify logic for useWatchQueryOnBlockChange so that the query is invalidated only on block change

--- packages/ui/src/hooks/useWatchQueryOnBlockChange.ts
+++ packages/ui/src/hooks/useWatchQueryOnBlockChange.ts
@@ -1,22 +1,16 @@
 import { useEffect, useRef } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import { useBlockNumber } from "wagmi";
-import { equals } from "ramda";
 
 export default function useWatchQueryOnBlockChange(
     queryKey: readonly unknown[],
 ) {
     const queryClient = useQueryClient();
     const { data: blockNumber } = useBlockNumber({ watch: true });
-    const lastQueryKey = useRef<readonly unknown[] | null>(null);
-    const lastBlockNumber = useRef<bigint | undefined>(undefined);
+    const lastBlockNumber = useRef<bigint | undefined>(blockNumber);
 
     useEffect(() => {
-        if (
-            !equals(queryKey, lastQueryKey.current) ||
-            blockNumber !== lastBlockNumber.current
-        ) {
-            lastQueryKey.current = queryKey;
+        if (blockNumber !== lastBlockNumber.current) {
             lastBlockNumber.current = blockNumber;
             void queryClient.invalidateQueries({ queryKey });
         }
