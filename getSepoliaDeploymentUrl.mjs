458a21fd3b70629f8ab7ddf8c68db529c3a27260
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-08-31 21:34:30 +0300

    test (apps/web): Test changes

--- getSepoliaDeploymentUrl.mjs
+++ getSepoliaDeploymentUrl.mjs
@@ -17,44 +17,41 @@ async function main() {
     let cartesiTeamId = null;
     let sepoliaDeploymentUrl = null;
 
-    return console.log(
-        "env::",
-        process.env.VERCEL_TOKEN,
-        process.env.GITHUB_SHA,
-    );
+    console.log("env::", process.env.VERCEL_TOKEN, process.env.GITHUB_SHA);
 
-    const vercel = new Vercel({
-        bearerToken: token,
-    });
-
-    try {
-        const teamsResult = await vercel.teams.getTeams({
-            limit: 20,
-        });
-
-        cartesiTeamId = teamsResult.teams.find(
-            (team) => team.slug === "cartesi",
-        ).id;
-
-        console.log("cartesiTeamId::", cartesiTeamId);
-
-        const deploymentsResult = await vercel.deployments.getDeployments({
-            app: "rollups-explorer",
-            teamId: cartesiTeamId,
-            sha: githubSha,
-        });
-
-        sepoliaDeploymentUrl = deploymentsResult?.url;
-
-        if (!sepoliaDeploymentUrl) {
-            throw new Error("Could not find deployment url for Sepolia");
-        }
-    } catch (error) {
-        console.error("Error while retrieving deployment data:", error);
-        process.exit(1);
-    }
-
-    return `https://${sepoliaDeploymentUrl}`;
+    // const vercel = new Vercel({
+    //     bearerToken: token,
+    // });
+    //
+    // try {
+    //     const teamsResult = await vercel.teams.getTeams({
+    //         limit: 20,
+    //     });
+    //
+    //     cartesiTeamId = teamsResult.teams.find(
+    //         (team) => team.slug === "cartesi",
+    //     ).id;
+    //
+    //     console.log("cartesiTeamId::", cartesiTeamId);
+    //
+    //     const deploymentsResult = await vercel.deployments.getDeployments({
+    //         app: "rollups-explorer",
+    //         teamId: cartesiTeamId,
+    //         sha: githubSha,
+    //     });
+    //
+    //     sepoliaDeploymentUrl = deploymentsResult?.url;
+    //
+    //     if (!sepoliaDeploymentUrl) {
+    //         throw new Error("Could not find deployment url for Sepolia");
+    //     }
+    // } catch (error) {
+    //     console.error("Error while retrieving deployment data:", error);
+    //     process.exit(1);
+    // }
+    //
+    // return `https://${sepoliaDeploymentUrl}`;
+    return "";
 }
 
 const url = await main();
