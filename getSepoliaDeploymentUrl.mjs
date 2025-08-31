2a864360c9ef21611d34f8d3b2a62dcd52c0b597
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-08-31 22:22:41 +0300

    test (apps/web): Test changes

--- getSepoliaDeploymentUrl.mjs
+++ getSepoliaDeploymentUrl.mjs
@@ -10,39 +10,38 @@ import { Vercel } from "@vercel/sdk";
  * @returns {Promise<string>}
  */
 async function main() {
-    const token = process.env.VERCEL_TOKEN;
+    const token = "pgGDj8AqcVTNBNcogCIULA6k"; // process.env.VERCEL_TOKEN;
     const githubSha = process.env.GITHUB_SHA;
     let sepoliaDeploymentUrl = null;
 
-    return `token: ${token}`;
-    // try {
-    //     const vercel = new Vercel({
-    //         bearerToken: token,
-    //     });
-    //
-    //     const teamsResult = await vercel.teams.getTeams({
-    //         limit: 20,
-    //     });
-    //
-    //     const cartesiTeamId = teamsResult.teams.find(
-    //         (team) => team.slug === "cartesi",
-    //     )?.id;
-    //
-    //     const deploymentsResult = await vercel.deployments.getDeployments({
-    //         app: "rollups-explorer-sepolia",
-    //         teamId: cartesiTeamId,
-    //         sha: githubSha,
-    //     });
-    //
-    //     const [latestDeployment] = deploymentsResult.deployments;
-    //
-    //     return latestDeployment.url;
-    // } catch (error) {
-    //     console.error("Error while retrieving deployment data:", error);
-    //     process.exit(1);
-    // }
-    //
-    // return `https://${sepoliaDeploymentUrl}`;
+    try {
+        const vercel = new Vercel({
+            bearerToken: token,
+        });
+
+        const teamsResult = await vercel.teams.getTeams({
+            limit: 20,
+        });
+
+        const cartesiTeamId = teamsResult.teams.find(
+            (team) => team.slug === "cartesi",
+        )?.id;
+
+        const deploymentsResult = await vercel.deployments.getDeployments({
+            app: "rollups-explorer-sepolia",
+            teamId: cartesiTeamId,
+            sha: githubSha,
+        });
+
+        const [latestDeployment] = deploymentsResult.deployments;
+
+        return latestDeployment.url;
+    } catch (error) {
+        console.error("Error while retrieving deployment data:", error);
+        process.exit(1);
+    }
+
+    return `https://${sepoliaDeploymentUrl}`;
 }
 
 const url = await main();
