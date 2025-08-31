aaa84554a2107a829914288aeb366cdc10d2eb35
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-08-31 22:11:28 +0300

    test (apps/web): Test changes

--- getSepoliaDeploymentUrl.mjs
+++ getSepoliaDeploymentUrl.mjs
@@ -6,45 +6,6 @@ import { Vercel } from "@vercel/sdk";
  */
 
 /**
- * @description Gets the deployment url for a given environment
- * @param token
- * @param githubSha
- * @param app
- * @returns {Promise<string>}
- */
-async function getDeploymentUrl(token, githubSha, app) {
-    const vercel = new Vercel({
-        bearerToken: token,
-    });
-
-    const teamsResult = await vercel.teams.getTeams({
-        limit: 20,
-    });
-
-    const cartesiTeamId = teamsResult.teams.find(
-        (team) => team.slug === "cartesi",
-    )?.id;
-
-    if (!cartesiTeamId) {
-        throw new Error("Could not find team id for Cartesi");
-    }
-
-    const deploymentsResult = await vercel.deployments.getDeployments({
-        app,
-        teamId: cartesiTeamId,
-        sha: githubSha,
-    });
-
-    const [latestDeployment] = deploymentsResult.deployments;
-
-    if (!latestDeployment?.url) {
-        throw new Error(`Could not find deployment url for ${app}`);
-    }
-
-    return latestDeployment.url;
-}
-
-/**
  * @description Gets the latest Sepolia deployment url
  * @returns {Promise<string>}
  */
@@ -53,49 +14,35 @@ async function main() {
     const githubSha = process.env.GITHUB_SHA;
     let sepoliaDeploymentUrl = null;
 
-    try {
-        // sepoliaDeploymentUrl = await getDeploymentUrl(
-        //     token,
-        //     githubSha,
-        //     "rollups-explorer-sepolia",
-        // );
-        const vercel = new Vercel({
-            bearerToken: token,
-        });
-
-        const teamsResult = await vercel.teams.getTeams({
-            limit: 20,
-        });
-
-        const cartesiTeamId = teamsResult.teams.find(
-            (team) => team.slug === "cartesi",
-        )?.id;
-
-        if (!cartesiTeamId) {
-            throw new Error("Could not find team id for Cartesi");
-        }
-
-        const deploymentsResult = await vercel.deployments.getDeployments({
-            app: "rollups-explorer-sepolia",
-            teamId: cartesiTeamId,
-            sha: githubSha,
-        });
-
-        const [latestDeployment] = deploymentsResult.deployments;
-
-        if (!latestDeployment?.url) {
-            throw new Error(
-                `Could not find deployment url for "rollups-explorer-sepolia"`,
-            );
-        }
-
-        return latestDeployment.url;
-    } catch (error) {
-        console.error("Error while retrieving deployment data:", error);
-        process.exit(1);
-    }
-
-    return `https://${sepoliaDeploymentUrl}`;
+    return `token: ${token}`;
+    // try {
+    //     const vercel = new Vercel({
+    //         bearerToken: token,
+    //     });
+    //
+    //     const teamsResult = await vercel.teams.getTeams({
+    //         limit: 20,
+    //     });
+    //
+    //     const cartesiTeamId = teamsResult.teams.find(
+    //         (team) => team.slug === "cartesi",
+    //     )?.id;
+    //
+    //     const deploymentsResult = await vercel.deployments.getDeployments({
+    //         app: "rollups-explorer-sepolia",
+    //         teamId: cartesiTeamId,
+    //         sha: githubSha,
+    //     });
+    //
+    //     const [latestDeployment] = deploymentsResult.deployments;
+    //
+    //     return latestDeployment.url;
+    // } catch (error) {
+    //     console.error("Error while retrieving deployment data:", error);
+    //     process.exit(1);
+    // }
+    //
+    // return `https://${sepoliaDeploymentUrl}`;
 }
 
 const url = await main();
