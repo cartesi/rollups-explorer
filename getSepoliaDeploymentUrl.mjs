0dc7122d04c13efebded85130810318ce79c88af
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-09-02 15:04:57 +0300

    Modify the e2e action to await the Sepolia Deployment (#372)
    Co-authored-by: Bruno Menezes <brunodmenezes@gmail.com>

---
+++ getSepoliaDeploymentUrl.mjs
@@ -0,0 +1,49 @@
+import { Vercel } from "@vercel/sdk";
+
+/**
+ * @description The script in this file is used by .github/workflows/e2e.yml
+ * to provide the sepolia deployment url
+ */
+
+/**
+ * @description Gets the latest Sepolia deployment url
+ * @returns {Promise<string>}
+ */
+async function main() {
+    const token = process.env.VERCEL_TOKEN;
+    const githubSha = process.env.GITHUB_SHA;
+
+    try {
+        const vercel = new Vercel({
+            bearerToken: token,
+        });
+
+        const teamsResult = await vercel.teams.getTeams({
+            limit: 20,
+        });
+
+        const cartesiTeamId = teamsResult.teams.find(
+            (team) => team.slug === "cartesi",
+        )?.id;
+
+        const deploymentsResult = await vercel.deployments.getDeployments({
+            app: "rollups-explorer-sepolia",
+            teamId: cartesiTeamId,
+            sha: githubSha,
+        });
+
+        const [latestDeployment] = deploymentsResult.deployments;
+
+        // The Property is called 'url', but currently it lacks an important part: the protocol.
+        return latestDeployment.url.includes("https://")
+            ? latestDeployment.url
+            : `https://${latestDeployment.url}`;
+    } catch (error) {
+        console.error("Error while retrieving deployment data:", error);
+        process.exit(1);
+    }
+}
+
+const url = await main();
+
+process.stdout.write(url);
