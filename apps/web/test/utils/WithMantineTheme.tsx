c91a3112837c8cbf6ee484d8eece58f3a3efe20a
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-09-11 11:00:13 +0300

    chore (apps/web): Refactoring

--- apps/web/test/utils/WithMantineTheme.tsx
+++ apps/web/test/utils/WithMantineTheme.tsx
@@ -1,15 +1,17 @@
 import { MantineProvider } from "@mantine/core";
-import { FC, FunctionComponent, ReactNode } from "react";
+import { FC, FunctionComponent, ReactNode, JSX } from "react";
 import { theme } from "../../src/providers/theme";
 
 export const withMantineTheme = <T,>(
     Component: FunctionComponent<T>,
 ): FC<T> => {
-    const NewComp: FC<T> = (props: T) => (
-        <MantineProvider defaultColorScheme="dark" theme={theme}>
-            {Component(props) as ReactNode}
-        </MantineProvider>
-    );
+    const NewComp: FC<T> = (props: T) => {
+        return (
+            <MantineProvider defaultColorScheme="dark" theme={theme}>
+                <Component {...(props as JSX.IntrinsicAttributes & T)} />
+            </MantineProvider>
+        );
+    };
 
     NewComp.displayName = Component.displayName;
 
