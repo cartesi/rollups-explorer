eede6e8d749ba4fbdbd85e26ffeb1d63a2099d8a
Bruno Menezes <brunodmenezes@gmail.com>
2025-12-17 19:29:57 +0000

    chore(dave): Add feature to log and export current node-api queries.
    * Make it easy to record and save the responses from the node-api. Also, applied a special treatment for BigInt to be able to parse it back later.

--- apps/dave/src/components/layout/Layout.tsx
+++ apps/dave/src/components/layout/Layout.tsx
@@ -4,19 +4,70 @@ import {
     AppShell,
     AppShellHeader,
     AppShellMain,
+    Button,
     Container,
     Group,
     useMantineTheme,
 } from "@mantine/core";
+import { notifications } from "@mantine/notifications";
+import { useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
-import type { FC, PropsWithChildren } from "react";
+import { defaultTo } from "ramda";
+import { Activity, type FC, type PropsWithChildren } from "react";
 import { useIsSmallDevice } from "../../hooks/useIsSmallDevice";
 import CartesiLogo from "../icons/CartesiLogo";
 
+const debugEnabled = defaultTo(
+    false,
+    process.env.NEXT_PUBLIC_DEBUG_ENABLED == "true",
+);
+
 const Layout: FC<PropsWithChildren> = ({ children }) => {
+    const queryClient = useQueryClient();
     const theme = useMantineTheme();
     const { isSmallDevice, viewport } = useIsSmallDevice();
     const { height } = viewport;
+
+    const printQueryInfo = () => {
+        const defaultOpts = queryClient.getDefaultOptions();
+        const queryCache = queryClient.getQueryCache();
+        console.log(defaultOpts);
+        console.log(queryCache);
+
+        notifications.show({
+            title: "Query cache printed",
+            message: "Check your browser devtools console.",
+        });
+    };
+
+    const logQueries = () => {
+        const queries = queryClient.getQueryCache().getAll();
+        const obj: object[] = [];
+        // @ts-expect-error saving whatever is the original to reset.
+        const orig = BigInt.prototype.toJSON;
+
+        // @ts-expect-error JSON.stringify will try to call toJSON on bigints.  ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt#use_within_json
+        BigInt.prototype.toJSON = function () {
+            return `$bigint:${this.toString()}`;
+        };
+
+        queries.forEach((query) => {
+            obj.push({ queryKey: query.queryKey, data: query.state.data });
+        });
+
+        try {
+            console.log(JSON.stringify(obj, null, 4));
+        } catch (error) {
+            console.error(error);
+        } finally {
+            // @ts-expect-error just adding the original back.
+            BigInt.prototype.toJSON = orig;
+            notifications.show({
+                title: "Queries as JSON requested!",
+                message: "Check your browser devtools console.",
+            });
+        }
+    };
 
     return (
         <AppShell>
@@ -30,6 +81,13 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
                     <Link href="/" aria-label="Home">
                         <CartesiLogo height={isSmallDevice ? 30 : 40} />
                     </Link>
+
+                    <Activity mode={debugEnabled ? "visible" : "hidden"}>
+                        <Group>
+                            <Button onClick={printQueryInfo}>Show cache</Button>
+                            <Button onClick={logQueries}>Export queries</Button>
+                        </Group>
+                    </Activity>
                 </Group>
             </AppShellHeader>
             <AppShellMain>
