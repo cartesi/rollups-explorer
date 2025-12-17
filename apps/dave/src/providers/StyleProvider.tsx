40f6d9a65d9a27f9f3f85aaf81583e407ccf6c65
Bruno Menezes <brunodmenezes@gmail.com>
2025-12-17 19:22:00 +0000

    feat(dave): Add env variables and notification system.

--- apps/dave/src/providers/StyleProvider.tsx
+++ apps/dave/src/providers/StyleProvider.tsx
@@ -1,10 +1,12 @@
 import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 import type { FC, PropsWithChildren } from "react";
 import theme from "./theme";
 
 export const StyleProvider: FC<PropsWithChildren> = ({ children }) => {
     return (
         <MantineProvider theme={theme} forceColorScheme="dark">
+            <Notifications />
             {children}
         </MantineProvider>
     );
