44a222b18afd037a512a43ff30f091c2988b141c
Bruno Menezes <brunodmenezes@gmail.com>
2026-01-13 17:48:18 +0000

    feat(dave): Add two-commitments use cases.

---
+++ apps/dave/src/providers/localdata/cases/app9/index.ts
@@ -0,0 +1,25 @@
+import type { Hex } from "viem";
+import { parseQueries } from "../../helpers";
+import type { DataInjector, UseCaseApplication } from "../../types";
+import queries from "./queries";
+
+export const appName = "AppNine";
+export const appAddress = "0xd2a9b49cad4cbaa04421e8731fdb917832bee281" as Hex;
+
+const setData: DataInjector = (queryClient) => {
+    const queryList = parseQueries(queries);
+
+    queryList.forEach((query) => {
+        queryClient.setQueryData(query.queryKey, query.data);
+    });
+
+    return queryClient;
+};
+
+const useCaseApp: UseCaseApplication = {
+    appAddress,
+    appName,
+    setData,
+};
+
+export default useCaseApp;
