e880633d07c5b2958c1728ceaeb9775a053784e7
Bruno Menezes <brunodmenezes@gmail.com>
2026-01-14 19:59:22 +0000

    feat(dave): Add three-commitments case [14]

---
+++ apps/dave/src/providers/localdata/cases/app14/index.ts
@@ -0,0 +1,25 @@
+import type { Hex } from "viem";
+import { parseQueries } from "../../helpers";
+import type { DataInjector, UseCaseApplication } from "../../types";
+import queries from "./queries";
+
+export const appName = "AppFourteen";
+export const appAddress = "0xe2a41abcd1bca20026e026c26088a799f6a912fa" as Hex;
+
+const setData: DataInjector = (queryClient) => {
+    const queryList = parseQueries(queries);
+
+    queryList.forEach((query) => {
+        queryClient.setQueryData(query.queryKey, query.data);
+    });
+
+    return queryClient;
+};
+
+const useCaseApp: UseCaseApplication = {
+    appAddress,
+    appName,
+    setData,
+};
+
+export default useCaseApp;
