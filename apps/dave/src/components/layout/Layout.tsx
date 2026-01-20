c37e18bad6a532ed5f1eb7caf001040c2a539e5c
Bruno Menezes <brunodmenezes@gmail.com>
2026-01-20 17:44:57 +0000

    feat(dave): Include wallet connect-button

--- apps/dave/src/components/layout/Layout.tsx
+++ apps/dave/src/components/layout/Layout.tsx
@@ -9,66 +9,66 @@ import {
     Group,
     useMantineTheme,
 } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import { notifications } from "@mantine/notifications";
-import { useQueryClient } from "@tanstack/react-query";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
-import { defaultTo } from "ramda";
 import { Activity, type FC, type PropsWithChildren } from "react";
 import { useIsSmallDevice } from "../../hooks/useIsSmallDevice";
+import { useAppConfig } from "../../providers/AppConfigProvider";
+import queryClient from "../../providers/queryClient";
 import CartesiLogo from "../icons/CartesiLogo";
 import { ThemeToggle } from "../ThemeToggle";
 
-const debugEnabled = defaultTo(
-    false,
-    process.env.NEXT_PUBLIC_DEBUG_ENABLED == "true",
-);
+const printQueryInfo = () => {
+    const defaultOpts = queryClient.getDefaultOptions();
+    const queryCache = queryClient.getQueryCache();
+    console.log(defaultOpts);
+    console.log(queryCache);
+
+    notifications.show({
+        title: "Query cache printed",
+        message: "Check your browser devtools console.",
+    });
+};
+
+const logQueries = () => {
+    const queries = queryClient.getQueryCache().getAll();
+    const obj: object[] = [];
+    // @ts-expect-error saving whatever is the original to reset.
+    const orig = BigInt.prototype.toJSON;
+
+    // @ts-expect-error JSON.stringify will try to call toJSON on bigints.  ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt#use_within_json
+    BigInt.prototype.toJSON = function () {
+        return `$bigint:${this.toString()}`;
+    };
+
+    queries.forEach((query) => {
+        obj.push({ queryKey: query.queryKey, data: query.state.data });
+    });
+
+    try {
+        console.log(JSON.stringify(obj, null, 4));
+    } catch (error) {
+        console.error(error);
+    } finally {
+        // @ts-expect-error just adding the original back.
+        BigInt.prototype.toJSON = orig;
+        notifications.show({
+            title: "Queries as JSON requested!",
+            message: "Check your browser devtools console.",
+        });
+    }
+};
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
-    const queryClient = useQueryClient();
     const theme = useMantineTheme();
     const { isSmallDevice, viewport } = useIsSmallDevice();
     const { height } = viewport;
-
-    const printQueryInfo = () => {
-        const defaultOpts = queryClient.getDefaultOptions();
-        const queryCache = queryClient.getQueryCache();
-        console.log(defaultOpts);
-        console.log(queryCache);
-
-        notifications.show({
-            title: "Query cache printed",
-            message: "Check your browser devtools console.",
-        });
-    };
-
-    const logQueries = () => {
-        const queries = queryClient.getQueryCache().getAll();
-        const obj: object[] = [];
-        // @ts-expect-error saving whatever is the original to reset.
-        const orig = BigInt.prototype.toJSON;
-
-        // @ts-expect-error JSON.stringify will try to call toJSON on bigints.  ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt#use_within_json
-        BigInt.prototype.toJSON = function () {
-            return `$bigint:${this.toString()}`;
-        };
-
-        queries.forEach((query) => {
-            obj.push({ queryKey: query.queryKey, data: query.state.data });
-        });
-
-        try {
-            console.log(JSON.stringify(obj, null, 4));
-        } catch (error) {
-            console.error(error);
-        } finally {
-            // @ts-expect-error just adding the original back.
-            BigInt.prototype.toJSON = orig;
-            notifications.show({
-                title: "Queries as JSON requested!",
-                message: "Check your browser devtools console.",
-            });
-        }
-    };
+    const hideBalanceViewport = useMediaQuery(
+        `(min-width:${theme.breakpoints.sm}) and (max-width:${50}em)`,
+    );
+    const { isMockEnabled, isDebugEnabled } = useAppConfig();
 
     return (
         <AppShell>
@@ -84,8 +84,11 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
                     </Link>
 
                     <Group>
+                        {!isMockEnabled && (
+                            <ConnectButton showBalance={!hideBalanceViewport} />
+                        )}
                         <ThemeToggle />
-                        <Activity mode={debugEnabled ? "visible" : "hidden"}>
+                        <Activity mode={isDebugEnabled ? "visible" : "hidden"}>
                             <Button onClick={printQueryInfo}>Show cache</Button>
                             <Button onClick={logQueries}>Export queries</Button>
                         </Activity>
