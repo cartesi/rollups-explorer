35a77ce8a5e309c063d3aa50f965344786d7cd69
Bruno Menezes <brunodmenezes@gmail.com>
2026-01-20 17:41:55 +0000

    feat(dave): Add rainbowkit wallet.

---
+++ apps/dave/src/lib/transportClient.ts
@@ -0,0 +1,22 @@
+import { isNotNilOrEmpty } from "ramda-adjunct";
+import { createClient, http } from "viem";
+import { type SupportedChain } from "./supportedChains";
+
+/**
+ * Returns a viem http client using the default chain http rpc or the one set on
+ * nodeRpcUrl parameter.
+ * @param {SupportedChain} chain
+ * @returns
+ */
+const createClientFor = (chain: SupportedChain, nodeRpcUrl?: string) => {
+    console.info(`Creating client transport for: ${chain.id} (${chain.name})`);
+
+    const [defaultRpcUrl] = chain.rpcUrls.default.http;
+    const transport = isNotNilOrEmpty(nodeRpcUrl)
+        ? http(nodeRpcUrl)
+        : http(defaultRpcUrl);
+
+    return createClient({ chain, transport });
+};
+
+export default createClientFor;
