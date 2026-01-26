36922facfa13972873673516b030186eb4ac65c2
Bruno Menezes <brunodmenezes@gmail.com>
2026-01-26 20:50:41 +0000

    refactor(dave): Move react dev patch to providers to avoid set use-client on root layout.

--- apps/dave/src/app/layout.tsx
+++ apps/dave/src/app/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { ColorSchemeScript, mantineHtmlProps } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
@@ -10,24 +9,6 @@ import { Providers } from "../providers/Providers";
 
 interface RootLayoutProps {
     children: ReactNode;
-}
-
-/**
- * This is a workaround to solve a problem when react-dom-client.development
- * is trying to stringify bigints.
- *
- * refer to issue {@link https://github.com/facebook/react/issues/35004}
- * a PR is currently open here {@link https://github.com/facebook/react/pull/35013}
- */
-if (process.env.NODE_ENV === "development") {
-    Object.defineProperty(BigInt.prototype, "toJSON", {
-        writable: false,
-        enumerable: true,
-        configurable: false,
-        value() {
-            return this.toString();
-        },
-    });
 }
 
 const RootLayout: FC<RootLayoutProps> = ({ children }) => {
