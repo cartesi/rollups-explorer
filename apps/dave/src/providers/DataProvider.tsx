17fa23a7d45a46bdda5f6f43dd8f7e0023d675e3
Bruno Menezes <brunodmenezes@gmail.com>
2026-02-12 15:47:58 +0000

    feat: On connection switch force a re-render by using react-key property based on connection id.

--- apps/dave/src/providers/DataProvider.tsx
+++ apps/dave/src/providers/DataProvider.tsx
@@ -1,22 +1,23 @@
 import { CartesiProvider } from "@cartesi/wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import type { FC, PropsWithChildren } from "react";
+import { useMemo, type FC, type PropsWithChildren } from "react";
 import { useSelectedNodeConnection } from "../components/connection/hooks";
 import queryClient from "./queryClient";
 import WalletProvider from "./WalletProvider";
 
-const newQueryClient = new QueryClient();
-
 const DataProvider: FC<PropsWithChildren> = ({ children }) => {
     const selectedConnection = useSelectedNodeConnection();
-
-    const client =
-        selectedConnection?.type === "system_mock"
-            ? queryClient
-            : newQueryClient;
+    const client = useMemo(
+        () =>
+            selectedConnection?.type === "system_mock"
+                ? queryClient
+                : new QueryClient(),
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [selectedConnection?.type, selectedConnection?.id],
+    );
 
     return (
-        <QueryClientProvider client={client}>
+        <QueryClientProvider client={client} key={selectedConnection?.id}>
             <CartesiProvider rpcUrl={selectedConnection?.url ?? ""}>
                 <WalletProvider>{children}</WalletProvider>
             </CartesiProvider>
