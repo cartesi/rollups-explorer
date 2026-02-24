4be4b59acaf7c26c2c97bbe7171dfc2b2b5abd0d
Bruno Menezes <brunodmenezes@gmail.com>
2026-02-24 19:14:14 +0000

    chore: bump nextJS version.
    * Apparently it fixed dev hot-reload problems.

--- apps/dave/eslint.config.mjs
+++ apps/dave/eslint.config.mjs
@@ -1,15 +1,16 @@
-import { nextJsConfig } from "eslint-config-cartesi/next-js";
+import { config as reactInternal } from "eslint-config-cartesi/react-internal";
 import reactRefresh from "eslint-plugin-react-refresh";
 import storybook from "eslint-plugin-storybook";
 import globals from "globals";
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
-    ...nextJsConfig,
+    ...reactInternal,
+    // ...nextJsConfig,
 
+    { ignores: ["coverage/**", ".turbo/**", "public/**", ".next/**"] },
     {
         files: ["**/*.{ts,tsx}"],
-        ignores: ["coverage/**", ".turbo/**", "public/**", ".next/**"],
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
