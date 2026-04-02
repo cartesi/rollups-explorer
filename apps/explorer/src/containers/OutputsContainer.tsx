c0d52e49dfa26def210e7b6f5858f64cef5973c5
Bruno Menezes <brunodmenezes@gmail.com>
2026-04-02 14:46:30 +0100

    refactor(explorer): container and page outputs to use the new components and hook.

--- apps/explorer/src/containers/OutputsContainer.tsx
+++ apps/explorer/src/containers/OutputsContainer.tsx
@@ -1,13 +1,22 @@
 "use client";
-import { useApplication } from "@cartesi/wagmi";
+import type { Pagination } from "@cartesi/viem";
+import { useApplication, useOutputs } from "@cartesi/wagmi";
 import { Anchor, Stack, Title } from "@mantine/core";
 import Link from "next/link";
-import { isNil, pathOr } from "ramda";
-import type { FC } from "react";
+import { useSearchParams } from "next/navigation";
+import { isNil, isNotNil, pathOr } from "ramda";
+import { useEffect, useMemo, useRef, type FC } from "react";
 import {
     Hierarchy,
     type HierarchyConfig,
 } from "../components/navigation/Hierarchy";
+import {
+    buildSearchFilter,
+    buildSearchLimit,
+    buildSearchOffset,
+    buildSearchSort,
+    outputSearchUrlQueryName,
+} from "../components/output/lib/outputSearchUtils";
 import { OutputsPage } from "../page/OutputsPage";
 import { pathBuilder } from "../routes/routePathBuilder";
 import { ContainerSkeleton } from "./ContainerSkeleton";
@@ -15,19 +24,60 @@ import ContainerStack from "./ContainerStack";
 
 export type OutputsContainerProps = {
     application: string;
-    descending?: boolean;
-    limit?: number;
-    offset?: number;
+};
+
+const HomeLink = () => (
+    <Anchor component={Link} href={pathBuilder.home()}>
+        Go back to Home
+    </Anchor>
+);
+
+const DisplayError: FC<{ message: string }> = ({ message }) => {
+    return (
+        <Stack align="center" justify="center" pt="xl">
+            <Title order={2}>{message}</Title>
+            <HomeLink />
+        </Stack>
+    );
 };
 
 export const OutputsContainer: FC<OutputsContainerProps> = (props) => {
+    const searchParams = useSearchParams();
+    const queryParams = useMemo(() => {
+        const { filterType, filterValue, sortValue, limitValue, offsetValue } =
+            outputSearchUrlQueryName;
+        const filter = buildSearchFilter({
+            filterType: searchParams.get(filterType),
+            filterValue: searchParams.get(filterValue),
+        });
+        const sort = buildSearchSort(searchParams.get(sortValue));
+        const limit = buildSearchLimit(searchParams.get(limitValue));
+        const offset = buildSearchOffset(searchParams.get(offsetValue));
+        return { filter, sort, limit, offset };
+    }, [searchParams]);
+
+    const {
+        data: outputs,
+        isLoading: loadingOutputs,
+        error: outputError,
+    } = useOutputs({
+        application: props.application,
+        outputType: queryParams.filter.type?.value,
+        limit: queryParams.limit,
+        offset: queryParams.offset,
+        descending: queryParams.sort.value === "desc",
+    });
+
     const {
         data: application,
-        isLoading,
-        error,
+        isLoading: loadingApplication,
+        error: applicationError,
     } = useApplication({
         application: props.application,
     });
+
+    const hasError = !isNil(applicationError) || !isNil(outputError);
+
     const hierarchyConfig: HierarchyConfig[] = [
         { title: "Home", href: "/" },
         {
@@ -40,32 +90,57 @@ export const OutputsContainer: FC<OutputsContainerProps> = (props) => {
         },
     ];
 
+    const fallbackPagination = useRef<Pagination>({
+        limit: queryParams.limit,
+        offset: queryParams.offset,
+        totalCount: 0,
+    });
+
+    useEffect(() => {
+        if (isNotNil(outputs?.pagination)) {
+            // keep last known pagination while in transit as the data becomes nil.
+            fallbackPagination.current = outputs.pagination;
+        }
+    }, [outputs?.pagination]);
+
     return (
         <ContainerStack>
             <Hierarchy hierarchyConfig={hierarchyConfig} />
 
-            {isNil(application) && isLoading ? (
+            {isNil(application) && loadingApplication ? (
                 <ContainerSkeleton />
-            ) : !isNil(application) ? (
+            ) : isNotNil(application) && !hasError ? (
                 <OutputsPage
-                    application={
-                        application?.applicationAddress ?? props.application
+                    application={application}
+                    pagination={
+                        outputs?.pagination ?? fallbackPagination.current
                     }
-                    limit={props.limit ?? 10}
+                    outputs={{
+                        data: outputs?.data ?? [],
+                        isLoading: loadingOutputs,
+                    }}
+                    limit={queryParams.limit}
+                    filter={queryParams.filter}
+                    sort={queryParams.sort}
+                />
+            ) : isNotNil(applicationError) ? (
+                <DisplayError
+                    message={pathOr(
+                        `Application ${props.application} not found`,
+                        ["details"],
+                        applicationError,
+                    )}
+                />
+            ) : isNotNil(outputError) ? (
+                <DisplayError
+                    message={pathOr(
+                        `Outputs for application ${props.application} not found`,
+                        ["details"],
+                        outputError,
+                    )}
                 />
             ) : (
-                <Stack align="center" justify="center" pt="xl">
-                    <Title order={2}>
-                        {pathOr(
-                            `Application ${props.application} not found`,
-                            ["details"],
-                            error,
-                        )}
-                    </Title>
-                    <Anchor component={Link} href={pathBuilder.home()}>
-                        Go back to Home
-                    </Anchor>
-                </Stack>
+                <DisplayError message="Unexpected error occurred" />
             )}
         </ContainerStack>
     );
