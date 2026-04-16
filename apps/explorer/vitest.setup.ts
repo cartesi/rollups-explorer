5a8ce37ca6766edb69758cded23e10a188f93102
Bruno Menezes <brunodmenezes@gmail.com>
2026-04-16 13:32:04 +0100

    chore(explorer): Add testing-library/react and dom dependencies and initial global setup.

--- apps/explorer/vitest.setup.ts
+++ apps/explorer/vitest.setup.ts
@@ -1,1 +1,8 @@
 // Add here any common setup for tests to avoid repetition in each file.
+import { cleanup } from "@testing-library/react";
+import { afterEach } from "vitest";
+
+// Automatically unmount and cleanup DOM after the test is finished.
+afterEach(() => {
+    cleanup();
+});
