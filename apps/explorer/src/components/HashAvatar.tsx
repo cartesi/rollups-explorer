c2205c45bcb547d04cbf1fbcd134b5b80a340066
Bruno Menezes <brunodmenezes@gmail.com>
2026-05-12 12:46:18 +0100

    refactor(explorer): Replace use of html id property with data-* property for custom data.

--- apps/explorer/src/components/HashAvatar.tsx
+++ apps/explorer/src/components/HashAvatar.tsx
@@ -16,5 +16,5 @@ const buildDataUrl = (hash: Hash): string =>
 
 export const HashAvatar: FC<HashAvatarProps> = ({ hash, ...props }) => {
     const src = useMemo(() => buildDataUrl(hash), [hash]);
-    return <Avatar id={`hash-avatar-${hash}`} src={src} {...props} />;
+    return <Avatar data-hash={`avatar-hash-${hash}`} src={src} {...props} />;
 };
