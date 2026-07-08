7e2b35f5c6451fe8453b8e68c27c8bc3a460cb64
Bruno Menezes <brunodmenezes@gmail.com>
2026-07-08 11:49:38 +0100

    feat(explorer): Add content/copy for output context.

---
+++ apps/explorer/src/content/outputMessages.ts
@@ -0,0 +1,27 @@
+/**
+ * This file contains the output messages used in the application.
+ */
+export const outputMessages = {
+    txhash: "Execution transaction hash",
+    delegateCallVoucherTxt: "Delegated Call Voucher",
+    voucherTxt: "Voucher",
+    noticeTxt: "Notice",
+    voucher: {
+        checking: "Checking voucher...",
+        preparing: "Preparing voucher...",
+        executed: "Executed",
+        execute: "Execute",
+        executionSuccess: "Voucher executed successfully!",
+        feedback: {
+            executionStatusTxt: "Voucher execution status",
+            executionStatusSuccess: "Executed successfully",
+            executionStatusError: "Execution failed",
+        },
+    },
+    epoch: {
+        nonAcceptedClaim:
+            "Once the epoch status become CLAIM_ACCEPTED the output will become executable.",
+        claimForeclosed:
+            "The epoch claim is foreclosed. The output cannot be executed.",
+    },
+} as const;
